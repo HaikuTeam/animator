a9372a020542face3bc68c93c62595559d4b3729
Zachary Brown <z@charybrown.com>
2017-01-05 15:33:30 -0800

    wip: copy in previous pass at import/deport

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { clone, merge } from 'lodash'
 import { argv } from 'yargs'
 import * as request from 'request'
 import * as chalk from 'chalk'
+import * as mkdirp from "mkdirp"
+import { execSync } from 'child_process'
 
 import { inkstone, client } from './sdk.js'
 
@@ -50,23 +52,49 @@ function help() {
   finish()
 }
 
+function ensureAuth(cb){
+  while(client.config.getAuthToken() == ""){
+    console.log("You must be authenticated to do that.")
+    doLogin()
+    if(client.config.getAuthToken() == ""){
+      console.log("Hm, that didn't work.  Let's try again.")
+    }
+  }
+  cb()
+}
+
 switch (subcommand) {
   case 'login':
-    login()
+    doLogin()
     break
   case 'logout':
   case 'logoff':
-    logout()
+    doLogout()
+    break
+  case 'import':
+    doImport()
     break
   case 'help':
     help()
+    break
+  case 'sync':
+    doSync()
     break
   default:
     help()
     break
 }
 
-function login() {
+function doImport(){
+  //TODO:
+  //  Parse args.
+  //  If not present, do interactive mode. (Future)
+  //  perform git subtree-foo
+  //  store a record in local .haiku kv store to keep track of imported project
+
+}
+
+function doLogin() {
   console.log(chalk.underline('Logging into Haiku'))
   var username = ''
   var password = ''
@@ -98,6 +126,43 @@ function login() {
   });
 }
 
-function logout(){
+function doLogout(){
   client.config.setAuthToken("")
+}
+
+//TODO:  realtime sync
+function doSync(){
+  console.warn("Unimplemented")
+}
+
+
+
+
+
+//USAGE:  haiku import design-test dest/
+//        clone git repo 'someendpoint/design-test' as a submodule into the dest/design-test folder
+//TODO:  update with url to our public infra (codecommit or similar)
+//TODO:  figure out auth (or do all public for now. +1 to hosting on our own infra)
+var GIT_CMD_BASE = "git@github.com:HaikuTeam/${1}.git"
+function performImport(projectName, destination)
+{
+  //TODO:  handle numerous edge cases (e.g. dest path does/not already have contents; remote not found)
+  if(destination.charAt(destination.length -1) !== "/") destination += '/'
+  mkdirp.sync(destination)
+  destination += projectName
+
+  var gitEndpoint = GIT_CMD_BASE.replace('${1}', projectName)
+  execSync(`git remote add ${projectName} ${gitEndpoint}`)
+  execSync(`git subtree add --prefix ${destination} ${projectName} master`)
+  
+}
+
+//USAGE:  haiku deport design-test dest/design-test
+//        removes subtree at dest/design-test
+//TODO:  still seems to be polluting git history. may need to follow additional steps re: reflog, see link
+//Could also explore submodules as a less history-intermingling solution (defeats part of the collaborative connection)
+function performDeport(projectName, location){
+  //command from http://stackoverflow.com/questions/26107798/how-to-remove-previously-added-git-subtree-and-its-history
+  var cmd = `git filter-branch --index-filter \'git rm --cached --ignore-unmatch -rf ${location}\' --prune-empty -f HEAD`
+  execSync(cmd)
 }
