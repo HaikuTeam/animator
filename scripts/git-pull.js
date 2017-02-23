2dc1f5c0859912e17affdab6e226f73e5aacd745
Matthew Trost <mrtrost@gmail.com>
2017-02-23 10:19:55 -0500

    tweak: branch/remote aware pull

--- scripts/git-pull.js
+++ scripts/git-pull.js
@@ -1,12 +1,16 @@
 var async = require('async')
 var cp = require('child_process')
+var argv = require('yargs').argv
 var log = require('./helpers/log')
 var allPackages = require('./helpers/allPackages')()
 
+var branch = argv.branch || 'master'
+var remote = argv.remote || 'origin'
+
 async.eachSeries(allPackages, function (pack, next) {
-  log.log('git pulling ' + pack.name)
+  log.log('git pulling ' + pack.name + ' (' + remote + ' ' + branch + ')')
   try {
-    cp.execSync('git pull', { cwd: pack.abspath })
+    cp.execSync('git pull ' + remote + ' ' + branch, { cwd: pack.abspath })
   } catch (exception) {
     log.log(exception.message)
   }
