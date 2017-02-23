dd1062a2d82a56f59e39b9c89b4865c5d63d1500
Matthew Trost <mrtrost@gmail.com>
2017-02-23 11:16:42 -0500

    fix: Dep graph based sha normalization

--- scripts/sha-norm.js
+++ scripts/sha-norm.js
@@ -1,24 +1,71 @@
-var async = require('async')
 var fse = require('fs-extra')
 var path = require('path')
+var lodash = require('lodash')
+var cp = require('child_process')
+var DepGraph = require('dependency-graph').DepGraph
 var log = require('./helpers/log')
 var allPackages = require('./helpers/allPackages')()
 
-async.eachSeries(allPackages, function (sourcePack, nextSource) {
-  async.eachSeries(allPackages, function (destPack, nextDest) {
-    if (!destPack.pkg) return nextDest()
-    if (!destPack.pkg.dependencies) return nextDest()
-    if (!destPack.pkg.dependencies[sourcePack.name]) return nextDest()
+var groups = lodash.keyBy(allPackages, 'name')
+var graph = new DepGraph()
 
-    var versionToUse = sourcePack.remote + '#' + sourcePack.sha
+allPackages.forEach(function (pack) {
+  graph.addNode(pack.name, pack)
+})
 
-    if (destPack.pkg.dependencies[sourcePack.name] === versionToUse) return nextDest()
+allPackages.forEach(function (pack) {
+  if (!pack.pkg) return void (0)
+  if (!pack.pkg.dependencies) return void (0)
+  for (var depName in pack.pkg.dependencies) {
+    // A few hacks...
+    if (depName.slice(0, 6) !== 'haiku-') continue
+    if (depName === 'haiku-fs-extra') continue
+    if (depName === 'haiku-creator-electron') depName = 'haiku-creator'
 
-    destPack.pkg.dependencies[sourcePack.name] = versionToUse
-    var packageContent = JSON.stringify(destPack.pkg, null, 2) + '\n'
-    fse.writeFileSync(path.join(destPack.abspath, 'package.json'), packageContent)
-    log.log('set ' + destPack.name + '\'s version of ' + sourcePack.name + ' to ' + versionToUse)
+    graph.addDependency(pack.name, depName)
+  }
+})
 
-    return nextDest()
-  }, nextSource)
+var order = graph.overallOrder()
+order.forEach(function (name) {
+  var pack = groups[name]
+  var deps = graph.dependenciesOf(name)
+  if (deps.length < 1) return void (0)
+  log.log(pack.name + ' wants ' + JSON.stringify(deps))
+
+  var didAnythingChange = false
+  var changedDeps = []
+  for (var i = 0; i < deps.length; i++) {
+    var depName = deps[i]
+    var depPack = groups[depName]
+    var depSha = depPack.sha
+    var depRemote = depPack.remote
+
+    if (depName === 'haiku-creator') depName = 'haiku-creator-electron'
+    var depUrl = depRemote + '#' + depSha
+
+    if (pack.pkg.dependencies[depName] !== depUrl) {
+      pack.pkg.dependencies[depName] = depUrl
+      log.log(pack.name + ' gets ' + depUrl)
+      changedDeps.push(depName)
+      didAnythingChange = true
+    }
+  }
+
+  if (didAnythingChange) {
+    log.log('saving, committing, and pushing ' + pack.name)
+    var commitMsg = 'chore: Updated dependency SHAs for ' + changedDeps.join(', ')
+    log.log('commit: ' + commitMsg)
+    var packJson = JSON.stringify(pack.pkg, null, 2) + '\n'
+
+    fse.writeFileSync(path.join(pack.abspath, 'package.json'), packJson)
+    log.log(cp.execSync('git add package.json', { cwd: pack.abspath }))
+    log.log(cp.execSync('git commit -m "' + commitMsg + '"', { cwd: pack.abspath }))
+    log.log(cp.execSync('git push origin HEAD', { cwd: pack.abspath }))
+
+    pack.sha = cp.execSync('git rev-parse HEAD', { cwd: pack.abspath }).toString().trim()
+    log.log('sha of ' + pack.name + ' is now ' + pack.sha)
+  } else {
+    log.log('no changes for ' + pack.name)
+  }
 })
