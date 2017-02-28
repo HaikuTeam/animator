ea72b083089c4163c6a2926202183b3695959106
Zachary Brown <z@charybrown.com>
2017-02-28 15:45:21 -0800

    devops: parallelize mono:npm-install

--- scripts/npm-install.js
+++ scripts/npm-install.js
@@ -1,14 +1,13 @@
-var async = require('async')
+var _ = require('lodash')
 var cp = require('child_process')
 var log = require('./helpers/log')
 var allPackages = require('./helpers/allPackages')()
 
-async.eachSeries(allPackages, function (pack, next) {
+_.forEach(allPackages, function (pack) {
   log.log('npm install for ' + pack.name)
   cp.exec('npm install', { cwd: pack.abspath }, function (err, out) {
     if (err) {
       log.err(err)
-      return next(err)
     }
     log.log(out)
 
@@ -18,21 +17,16 @@ async.eachSeries(allPackages, function (pack, next) {
       return cp.exec('git submodule update --init --recursive', { cwd: pack.abspath }, function (err, out) {
         if (err) {
           log.err(err)
-          return next(err)
         }
         log.log(out)
         log.log('compiling javascript for ' + pack.name)
         return cp.exec('npm run compile', { cwd: pack.abspath }, function (err, out) {
           if (err) {
             log.err(err)
-            return next(err)
           }
           log.log(out)
-          return next()
         })
       })
     }
-
-    return next()
   })
 })
