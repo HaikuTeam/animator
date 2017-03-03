7bb92a943af7062925ae40909e23e2a26528f925
Matthew Trost <mrtrost@gmail.com>
2017-03-03 07:34:51 -0500

    feat: mono:git-diff script

---
+++ scripts/git-diff.js
@@ -0,0 +1,13 @@
+var async = require('async')
+var cp = require('child_process')
+var log = require('./helpers/log')
+var allPackages = require('./helpers/allPackages')()
+
+async.eachSeries(allPackages, function (pack, next) {
+  try {
+    cp.execSync('git diff', { cwd: pack.abspath, stdio: 'inherit' })
+  } catch (exception) {
+    log.log(exception.message)
+  }
+  return next()
+})
