b9f3c68df30ddfd3f228e9b375272c9d4121924e
Matthew Trost <mrtrost@gmail.com>
2017-03-27 16:15:58 -0400

    chore: Pull in SignUp proj as demo for xbrowser and perf testing

---
+++ demo/sign-up-button/haiku.js
@@ -0,0 +1,9 @@
+module.exports = {
+  type: 'haiku',
+  name: 'SignUp',
+  interpreter: 'SignUpInterpreter.js',
+  bytecode: 'SignUpBytecode.js',
+  react: {
+    component: 'SignUpComponent'
+  }
+}
