7184b206d0073ee5c9ccff986446aca4736eaa3b
Matthew Trost <mrtrost@gmail.com>
2017-03-28 14:47:48 -0400

    tweak: Track explicitly set styles so they don't get overwritten later

--- src/renderers/dom/assignStyle.js
+++ src/renderers/dom/assignStyle.js
@@ -1,8 +1,12 @@
 function assignStyle (domElement, style, options, scopes) {
+  if (!domElement.__haikuExplicitStyles) domElement.__haikuExplicitStyles = {}
   for (var key in style) {
     var newProp = style[key]
     var previousProp = domElement.style[key]
-    if (previousProp !== newProp) domElement.style[key] = style[key]
+    if (previousProp !== newProp) {
+      domElement.__haikuExplicitStyles[key] = true
+      domElement.style[key] = style[key]
+    }
   }
   return domElement
 }
