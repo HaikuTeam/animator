d1fa7c4ce8b625e9cbc112dd04e70e2f3b795915
Matthew Trost <mrtrost@gmail.com>
2017-03-29 09:09:43 -0400

    tweak: Don't default these args

--- src/renderers/dom/render.js
+++ src/renderers/dom/render.js
@@ -1,7 +1,7 @@
 var renderTree = require('./renderTree')
 
 function render (domElement, virtualContainer, virtualTree, locator, hash, options, scopes) {
-  return renderTree(domElement, virtualContainer, [virtualTree], locator, hash || {}, options || {}, scopes || {})
+  return renderTree(domElement, virtualContainer, [virtualTree], locator, hash, options, scopes)
 }
 
 module.exports = render
