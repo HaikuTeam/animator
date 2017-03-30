eeb190a86df657d0ad5b37f7890c04c8c1750015
Matthew Trost <mrtrost@gmail.com>
2017-03-30 15:57:30 -0400

    stuff

---
+++ scripts/git-checkout.js
@@ -0,0 +1,18 @@
+var async = require('async')
+var cp = require('child_process')
+var argv = require('yargs').argv
+var log = require('./helpers/log')
+var allPackages = require('./helpers/allPackages')()
+
+var branch = argv.branch
+if (!branch) throw new Error('Provide a branch, please!')
+
+async.eachSeries(allPackages, function (pack, next) {
+  log.log('git checkout ' + branch + ' . in ' + pack.name)
+  try {
+    cp.execSync('git checkout ' + branch, { cwd: pack.abspath })
+  } catch (exception) {
+    log.log(exception.message)
+  }
+  return next()
+})
