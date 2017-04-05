675a483d8aa7cdfce09eda45f66187a4346ec67f
Matthew Trost <mrtrost@gmail.com>
2017-04-05 19:35:06 -0400

    tweak: Check contents of window object since this is not nec dom

--- src/renderers/dom/isIE.js
+++ src/renderers/dom/isIE.js
@@ -1,3 +1,3 @@
 module.exports = function isIE (window) {
-  return new RegExp('MSIE ([0-9]{1,}[.0-9]{0,})').exec(window.navigator.userAgent) !== null ? parseFloat(RegExp.$1) : false
+  return new RegExp('MSIE ([0-9]{1,}[.0-9]{0,})').exec(window && window.navigator && window.navigator.userAgent) !== null ? parseFloat(RegExp.$1) : false
 }
