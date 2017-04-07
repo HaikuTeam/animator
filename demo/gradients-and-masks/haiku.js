c4c9f2ea10cc3d45042b63b965175e4b2a86a55d
Matthew Trost <mrtrost@gmail.com>
2017-04-07 10:41:41 -0400

    fix: Add demo of gradients and masks (frag ident issue)

---
+++ demo/gradients-and-masks/haiku.js
@@ -0,0 +1,9 @@
+module.exports = {
+  type: 'haiku',
+  name: 'UsersMatthewCodeHaikuTeamMonoPackagesHaikuPlumbingTestFixturesProjectsBlankProject',
+  interpreter: 'UsersMatthewCodeHaikuTeamMonoPackagesHaikuPlumbingTestFixturesProjectsBlankProjectInterpreter.js',
+  bytecode: 'UsersMatthewCodeHaikuTeamMonoPackagesHaikuPlumbingTestFixturesProjectsBlankProjectBytecode.js',
+  react: {
+    component: 'UsersMatthewCodeHaikuTeamMonoPackagesHaikuPlumbingTestFixturesProjectsBlankProjectComponent'
+  }
+}
