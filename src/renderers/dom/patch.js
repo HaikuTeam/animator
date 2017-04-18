da7dd36c558d3468cf071d9ca42148686dba9edf
Matthew Trost <mrtrost@gmail.com>
2017-04-18 08:10:35 -0400

    feat: Allow a modifier function to replace elements on the fly

--- src/renderers/dom/patch.js
+++ src/renderers/dom/patch.js
@@ -20,6 +20,14 @@ function patch (topLevelDomElement, virtualContainer, patchesDict, locator, hash
   if (Object.keys(patchesDict) < 1) return topLevelDomElement
   for (var flexId in patchesDict) {
     var virtualElement = patchesDict[flexId]
+
+    if (virtualElement && options.modifier) {
+      var virtualReplacement = options.modifier(virtualElement)
+      if (virtualReplacement !== undefined) {
+        virtualElement = virtualReplacement
+      }
+    }
+
     var domElement = getElementByFlexId(topLevelDomElement, flexId, scopes)
     if (domElement) {
       updateElement(domElement, virtualElement, domElement.parentNode, virtualElement.__parent, domElement.haiku.locator, hash, options, scopes)
