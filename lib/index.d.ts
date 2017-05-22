8e4c62097734aa310efdcdf9b67b7c4a60497c8d
Zachary Brown <z@charybrown.com>
2017-05-21 17:21:16 -0700

    hack: add lib to git

---
+++ lib/index.d.ts
@@ -0,0 +1,22 @@
+export declare namespace client {
+    function verboselyLog(message: string, ...args: any[]): void;
+    function error(err: any): void;
+    class npm {
+        static readPackageJson(path?: string): any;
+        static writePackageJson(jsonObject: any, path?: string): void;
+    }
+    class git {
+        static ensureRemoteIsInitialized(remoteName: string, remoteUrl: string, cb: Function): void;
+        static forciblyCloneSubrepo(remote: string, path: string, cb: Function): void;
+        static cloneRepo(remote: string, path: string, cb: (error?: any) => any): void;
+    }
+    interface ClientConfig {
+        verbose?: boolean;
+    }
+    function setConfig(newVals: ClientConfig): void;
+    class config {
+        static getAuthToken(): string;
+        static isAuthenticated(): boolean;
+        static setAuthToken(newToken: string): void;
+    }
+}
