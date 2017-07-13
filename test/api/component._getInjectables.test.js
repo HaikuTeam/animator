d94c8259f29fd4843e93e6b80c8735ffc47a1271
Matthew Trost <mrtrost@gmail.com>
2017-07-13 07:48:33 -0400

    test: Some quick api tests

---
+++ test/api/component._getInjectables.test.js
@@ -0,0 +1,71 @@
+var test = require('tape')
+var PACKAGE = require('./../../package.json')
+var TestHelpers = require('./../TestHelpers')
+var HaikuDOMAdapter = require('./../../src/adapters/dom')
+test('component._getInjectables', function (t) {
+  t.plan(13)
+
+  var bytecode = {
+    // Checking that the bytecode itself can define options
+    options: {
+      contextMenu: 'foobar123'
+    },
+    states: {
+      baz: {
+        value: 'abc'
+      }
+    },
+    timelines: {
+      Default: {
+        'haiku:abcdefghijk': {
+          opacity: {
+            0: {
+              value: 1
+            }
+          }
+        }
+      }
+    },
+    template: {
+      elementName: 'div',
+      attributes: { 'haiku-id': 'abcdefghijk' },
+      children: []
+    }
+  }
+
+  TestHelpers.createDOM(function (err, win) {
+    // Combo passing options at root and nested
+    var HaikuComponentFactory = HaikuDOMAdapter(bytecode, {}, win)
+
+    // Pass loop at root level
+    var component = HaikuComponentFactory(mount, {
+      states: {
+        bux: {
+          value: 9000
+        }
+      }
+    })
+
+    var injectables = component._getInjectables()
+
+    t.ok(injectables.baz)
+    t.equal(injectables.baz, 'string')
+
+    t.ok(injectables.bux)
+    t.equal(injectables.bux, 'number')
+
+    t.ok(injectables.$global)
+    t.ok(injectables.$player)
+    t.ok(injectables.$component)
+    t.ok(injectables.$root)
+
+    t.ok(injectables.$element)
+    t.ok(injectables.$element.properties)
+
+    t.ok(injectables.$tree)
+    t.ok(injectables.$flow)
+    t.ok(injectables.$user)
+
+    component._context.clock._cancelRaf()
+  })
+})
