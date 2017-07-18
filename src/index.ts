f5541797c19a31cf1c727f8dfee37ae6d166d85e
Matthew Trost <mrtrost@gmail.com>
2017-07-18 14:57:31 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { client } from "haiku-sdk-client"
 let dedent = require("dedent")
 
 const banner = dedent`
-  Haiku CLI (version 2.1.17)
+  Haiku CLI (version 2.1.18)
 
   Usage:
     haiku <command> [flags]
