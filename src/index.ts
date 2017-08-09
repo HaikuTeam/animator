149f9945d197a9a9f09eee8a432100510d21c760
Taylor Poe <taylorpoe@gmail.com>
2017-08-09 16:10:12 -0700

    "auto: Housekeeping"

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { client } from "haiku-sdk-client"
 let dedent = require("dedent")
 
 const banner = dedent`
-  Haiku CLI (version 2.1.28)
+  Haiku CLI (version 2.1.29)
 
   Usage:
     haiku <command> [flags]
