6fc8579b78e8aaf1955a5ab53fc7059d487ac344
Matthew Trost <mrtrost@gmail.com>
2017-08-18 10:32:09 -0400

    fix: When doing the type check, don't make the classic blunder

--- src/HaikuComponent.js
+++ src/HaikuComponent.js
@@ -548,10 +548,24 @@ function _typecheckStateSpec (stateSpec, stateSpecName) {
     return void 0
   }
 
-  if (typeof stateSpec.value !== stateSpec.type) {
-    throw new Error(
-      'Property value `' + stateSpecName + '` must be a `' + stateSpec.type + '`'
-    )
+  if (stateSpec.type === 'array') {
+    if (!Array.isArray(stateSpec.value)) {
+      throw new Error(
+        'Property value `' + stateSpecName + '` must be an array'
+      )
+    }
+  } else if (stateSpec.type === 'object') {
+    if (stateSpec.value && typeof stateSpec.value !== 'object') {
+      throw new Error(
+        'Property value `' + stateSpecName + '` must be an object'
+      )
+    }
+  } else {
+    if (typeof stateSpec.value !== stateSpec.type) {
+      throw new Error(
+        'Property value `' + stateSpecName + '` must be a `' + stateSpec.type + '`'
+      )
+    }
   }
 }
 
