232b6a5d97eb8b643905f5c31f1f2522faf9714a
Zachary Brown <z@charybrown.com>
2017-08-30 12:02:58 -0700

    "auto: Housekeeping"

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { client } from "haiku-sdk-client"
 let dedent = require("dedent")
 
 const banner = dedent`
-  Haiku CLI (version 2.1.47)
+  Haiku CLI (version 2.1.48)
 
   Usage:
     haiku <command> [flags]
