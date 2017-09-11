235db6bb147789c916a97b4f09c49990e08a517b
Matthew Trost <mrtrost@gmail.com>
2017-09-11 15:24:11 -0400

    test: Update test with new assets input format

--- packages/haiku-plumbing/test/unit/Asset.assetsToDirectoryStructure.test.js
+++ packages/haiku-plumbing/test/unit/Asset.assetsToDirectoryStructure.test.js
@@ -4,14 +4,41 @@ var File = require('haiku-serialization/src/model/File')()
 var Asset = require('./../../lib/Asset')
 
 test('Asset.assetsToDirectoryStructure', function(t) {
-  t.plan(5)
-
-  return File.ingestFromFolder(path.join(__dirname, '..', 'fixtures', 'projects', 'test-project'), {}, (err, files) => {
-    t.ok(!err, 'no error ingesting')
-    var dir = Asset.assetsToDirectoryStructure(files)
-    t.ok(dir[0].type === 'sketch')
-    t.ok(dir[0].artboards.type === 'folder')
-    t.ok(dir[0].slices.type === 'folder')
-    t.ok(dir[0].pages.type === 'folder')
+  t.plan(4)
+  var dir = Asset.assetsToDirectoryStructure({
+    'designs/TEST.sketch': {
+      relpath: 'designs/TEST.sketch',
+      abspath: path.join(__dirname, '..', 'projects', 'test-project', 'designs/TEST.sketch'),
+      dtModified: Date.now()
+    },
+    'designs/TEST.sketch.contents/artboards/Another Artboard.svg': {
+      relpath: 'designs/TEST.sketch.contents/artboards/Another Artboard.svg',
+      abspath: path.join(__dirname, '..', 'projects', 'test-project', 'designs/TEST.sketch.contents/artboards/Another Artboard.svg'),
+      dtModified: Date.now()
+    },
+    'designs/TEST.sketch.contents/artboards/Artboard.svg': {
+      relpath: 'designs/TEST.sketch.contents/artboards/Artboard.svg',
+      abspath: path.join(__dirname, '..', 'projects', 'test-project', 'designs/TEST.sketch.contents/artboards/Artboard.svg'),
+      dtModified: Date.now()
+    },
+    'designs/TEST.sketch.contents/pages/Page 1.svg': {
+      relpath: 'designs/TEST.sketch.contents/pages/Page 1.svg',
+      abspath: path.join(__dirname, '..', 'projects', 'test-project', 'designs/TEST.sketch.contents/pages/Page 1.svg'),
+      dtModified: Date.now()
+    },
+    'designs/TEST.sketch.contents/slices/Dicey.svg': {
+      relpath: 'designs/TEST.sketch.contents/slices/Dicey.svg',
+      abspath: path.join(__dirname, '..', 'projects', 'test-project', 'designs/TEST.sketch.contents/slices/Dicey.svg'),
+      dtModified: Date.now()
+    },
+    'designs/TEST.sketch.contents/slices/Slicey.svg': {
+      relpath: 'designs/TEST.sketch.contents/slices/Slicey.svg',
+      abspath: path.join(__dirname, '..', 'projects', 'test-project', 'designs/TEST.sketch.contents/slices/Slicey.svg'),
+      dtModified: Date.now()
+    },
   })
+  t.ok(dir[0].type === 'sketch')
+  t.ok(dir[0].artboards.type === 'folder')
+  t.ok(dir[0].slices.type === 'folder')
+  t.ok(dir[0].pages.type === 'folder')
 })
