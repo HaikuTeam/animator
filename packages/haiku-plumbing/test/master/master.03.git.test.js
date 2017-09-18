228905f9100bd3dc1fef44ebe782fd4041ef2d33
Matthew Trost <mrtrost@gmail.com>
2017-09-18 11:27:40 -0400

    test: Update test with new undoable count after small change to save/update flow

--- packages/haiku-plumbing/test/master/master.03.git.test.js
+++ packages/haiku-plumbing/test/master/master.03.git.test.js
@@ -13,19 +13,19 @@ tape('master.03.git', (t) => {
       function (cb) { return master.initializeFolder({ params: ['boobar', 'matthew+matthew@haiku.ai', 'supersecure', {}] }, cb) },
       function (cb) { return master.startProject({}, cb) },
       function (cb) {
-        t.equal(master._git._gitUndoables.length, 2, 'two git undoables')
+        t.equal(master._git._gitUndoables.length, 1, '1 git undoable')
         return cb()
       },
       function (cb) {
         fse.writeFileSync(path.join(folder, 'Hello.svg'), '<svg><rect x="0" y="0" stroke="1" fill="black"></rect></svg>')
         setTimeout(() => {
-          t.equal(master._git._gitUndoables.length, 3, 'three git undoables')
+          t.equal(master._git._gitUndoables.length, 2, '2 git undoables')
           return master.instantiateComponent({ params: ['Hello.svg', {}] }, cb)
         }, 2000)
       },
       function (cb) {
         setTimeout(() => {
-          t.equal(master._git._gitUndoables.length, 4, 'four git undoables')
+          t.equal(master._git._gitUndoables.length, 3, '3 git undoables')
           return cb()
         }, 2000)
       }
