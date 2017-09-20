ee528c70dc6fe709b565cdbc9c45aa4e0611f068
Roberto Dip <dip.jesusr@gmail.com>
2017-09-20 10:44:51 -0300

    Add more videos to some of the tour steps

--- packages/haiku-creator/react/components/Tour/Steps/PublishedLink.js
+++ packages/haiku-creator/react/components/Tour/Steps/PublishedLink.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default function ({ styles }) {
   return (
     <div>
+      <video height="102" width="222" autoPlay loop muted playsInline preload="true">
+        <source src="https://giant.gfycat.com/NeglectedFancyImago.mp4" type="video/mp4" />
+        <img src="https://thumbs.gfycat.com/NeglectedFancyImago-small.gif" height="102" width="222" />
+      </video>
       <h2 style={styles.heading}>Publishing</h2>
       <div style={styles.text}>
         <p>
--- packages/haiku-creator/react/components/Tour/Steps/StatesStart.js
+++ packages/haiku-creator/react/components/Tour/Steps/StatesStart.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default function ({ styles }) {
   return (
     <div>
+      <video height="90" width="182" autoPlay loop muted playsInline preload="true">
+        <source src="https://giant.gfycat.com/YoungZanyArgentinehornedfrog.mp4" type="video/mp4" />
+        <img src="https://thumbs.gfycat.com/YoungZanyArgentinehornedfrog-small.gif" height="90" width="182" />
+      </video>
       <h2 style={styles.heading}>States</h2>
       <div style={styles.text}>
         <p>
