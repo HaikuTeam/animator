f46afbd7891f532ba0977ffa415b1ebdbf9dedd7
Roberto Dip <dip.jesusr@gmail.com>
2017-09-20 14:55:56 -0300

    Update the copy for the library step

--- packages/haiku-creator/react/components/Tour/Steps/LibraryStart.js
+++ packages/haiku-creator/react/components/Tour/Steps/LibraryStart.js
@@ -6,15 +6,12 @@ export default function ({ styles }) {
       <h2 style={styles.heading}>Library</h2>
       <div style={styles.text}>
         <p>Let's focus on the library now.</p>
-        <p>
-          You can drag any element from here to the stage to instantiate it.
-        </p>
-        <p>
-          But let's try demonstrating the connected design and animation
-          workflows first. Note the "checkmark" element, let's change its color
-          in Sketch and watch it update here in Haiku automatically.
-          Double click the Sketch Diamond to open Sketch.
-        </p>
+
+        <p>You can drag any element from here to the stage to instantiate it.</p>
+
+        <p>But let's check out the connected design and animation workflow first. Notice the “checkmark” asset here, which is instantiated on the stage.  Let's change its color in Sketch and watch it update here in Haiku automatically.</p>
+
+        <p><strong>Double-click the Sketch file to launch Sketch.</strong></p>
       </div>
     </div>
   )
