d9615609f473b959e95b742d870daecc4953634a
Roberto Dip <dip.jesusr@gmail.com>
2017-09-20 10:00:52 -0300

    Add a new file protocol to serve static files

--- packages/haiku-creator/electron.js
+++ packages/haiku-creator/electron.js
@@ -3,6 +3,7 @@ var electron = require('electron')
 var ipcMain = electron.ipcMain
 var systemPreferences = electron.systemPreferences
 var app = electron.app
+var protocol = electron.protocol
 var EventEmitter = require('events').EventEmitter
 var util = require('util')
 var TopMenu = require('./TopMenu')
@@ -215,6 +216,14 @@ if (app.isReady()) {
   createWindow()
 } else {
   app.on('ready', createWindow)
+  app.on('ready', () => {
+    protocol.registerFileProtocol('creator', (request, callback) => {
+      const url = request.url.substr(7)
+      callback({path: path.normalize(`${__dirname}/${url}`)})
+    }, (error) => {
+      if (error) console.error('Failed to register protocol')
+    })
+  })
 }
 
 // Hacky: When plumbing launches inside an Electron process it expects an EventEmitter-like
--- packages/haiku-creator/react/components/Tour/Steps/ScrubTicker.js
+++ packages/haiku-creator/react/components/Tour/Steps/ScrubTicker.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function ({ styles }) {
   return (
     <div>
-      <img src='http://placehold.it/250x100' alt='' />
+      <video height='220' width='460' autoPlay='' loop='' muted='' playsInline='' preload='true'>
+        <source src='creator:///assets/scrub_ticker.webm' type='video/webm' />
+      </video>
       <h2 style={styles.heading}>Travel in time</h2>
       <p style={styles.text}>Haikus are built on timelines. Scrub the ticker to move through time and leave it on frame 15.</p>
     </div>
