2968f881838bf9b39544fee858a48f19d3b4046e
Matthew Trost <mrtrost@gmail.com>
2017-09-20 13:26:31 -0400

    chore: Add/revise some of the test projects

---
+++ packages/haiku-glass/test/projects/comet-rotation/code/main/code.js
@@ -0,0 +1,241 @@
+var Haiku = require("@haiku/player");
+module.exports = {
+  metadata: {
+    uuid: "HAIKU_SHARE_UUID",
+    type: "haiku",
+    name: "UsersMatthewCodeHaikuTeamMonoPackagesHaikuPlumbingTestFixturesProjectsBlankProject",
+    relpath: "code/main/code.js"
+  },
+
+  options: {},
+  states: {},
+  eventHandlers: {},
+  timelines: {
+    Default: {
+      "haiku:0e366ff49e89": {
+        "style.WebkitTapHighlightColor": { "0": { value: "rgba(0,0,0,0)" } },
+        "style.position": { "0": { value: "relative" } },
+        "style.overflowX": { "0": { value: "hidden" } },
+        "style.overflowY": { "0": { value: "hidden" } },
+        "sizeAbsolute.x": { "0": { value: 550 } },
+        "sizeAbsolute.y": { "0": { value: 400 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "sizeMode.z": { "0": { value: 1 } }
+      },
+      "haiku:364b522f7643": {
+        viewBox: { "0": { value: "0 0 322 192" } },
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 322 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 192 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: 123, edited: true } },
+        "translation.y": { "0": { value: 95, edited: true } },
+        "style.zIndex": { "0": { value: 1 } },
+        "rotation.x": { "0": { value: 0, edited: true } },
+        "rotation.y": { "0": { value: 1, edited: true } },
+        "rotation.z": { "0": { value: 3.6049668520397353, edited: true } }
+      },
+      "haiku:f8ff3c173459": {
+        x1: { "0": { value: "20.4299463%" } },
+        y1: { "0": { value: "77.8755104%" } },
+        x2: { "0": { value: "207.604035%" } },
+        y2: { "0": { value: "129.15415%" } }
+      },
+      "haiku:289095f7d802": {
+        "stop-color": { "0": { value: "#B4EC51" } },
+        "stop-opacity": { "0": { value: "0.01" } },
+        offset: { "0": { value: "0%" } }
+      },
+      "haiku:898271de5df3": {
+        "stop-color": { "0": { value: "#8CCD40" } },
+        offset: { "0": { value: "47.1270129%" } }
+      },
+      "haiku:c61620123879": {
+        "stop-color": { "0": { value: "#429321" } },
+        offset: { "0": { value: "100%" } }
+      },
+      "haiku:e39355aee3d3": {
+        x: { "0": { value: "-10.8%" } },
+        y: { "0": { value: "-18.8%" } },
+        filterUnits: { "0": { value: "objectBoundingBox" } },
+        "sizeProportional.x": { "0": { value: 1.216 } },
+        "sizeMode.x": { "0": { value: 0 } },
+        "sizeProportional.y": { "0": { value: 1.376 } },
+        "sizeMode.y": { "0": { value: 0 } }
+      },
+      "haiku:8d762501b27a": {
+        stdDeviation: { "0": { value: "10.4101562" } },
+        in: { "0": { value: "SourceGraphic" } }
+      },
+      "haiku:3d342fe86b5d": {
+        x1: { "0": { value: "0%" } },
+        y1: { "0": { value: "23.1707317%" } },
+        x2: { "0": { value: "100%" } },
+        y2: { "0": { value: "69.4359756%" } }
+      },
+      "haiku:232afdd53506": {
+        "stop-color": { "0": { value: "#B4EC51" } },
+        offset: { "0": { value: "0%" } }
+      },
+      "haiku:e6ffc1dd8570": {
+        "stop-color": { "0": { value: "#429321" } },
+        offset: { "0": { value: "100%" } }
+      },
+      "haiku:708ddb7f4e0b": {
+        stroke: { "0": { value: "none" } },
+        "stroke-width": { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        "fill-rule": { "0": { value: "evenodd" } }
+      },
+      "haiku:cb64161468a9": {
+        "translation.x": { "0": { value: 2 } },
+        "translation.y": { "0": { value: 24 } }
+      },
+      "haiku:21aff5934d8b": {
+        d: {
+          "0": {
+            value: "M0.0121442605,110.989315 C32.6726325,44.4153995 91.4572787,0 158.547184,0 C209.340511,0 255.373342,25.458483 288.913011,66.71688 L234.02192,121.607971 C213.727164,100.018624 187.416928,86.9887465 158.670229,86.9887465 C113.520417,86.9887465 74.3809003,119.130995 55.1181992,166.09537 L0.0121442605,110.989315 Z"
+          }
+        },
+        fill: { "0": { value: "url(#linearGradient-1-8ecf01)" } },
+        filter: { "0": { value: "url(#filter-2-8ecf01)" } }
+      },
+      "haiku:f18f63f54bb9": {
+        fill: { "0": { value: "url(#linearGradient-3-8ecf01)" } },
+        cx: { "0": { value: "281.5" } },
+        cy: { "0": { value: "107" } },
+        rx: { "0": { value: "37.5" } },
+        ry: { "0": { value: "46" } },
+        "translation.x": { "0": { value: -16.1 } },
+        "translation.y": { "0": { value: 60.87 } },
+        "rotation.z": { "0": { value: 6.07 } }
+      }
+    }
+  },
+
+  template: {
+    elementName: "div",
+    attributes: {
+      "haiku-title": "UsersMatthewCodeHaikuTeamMonoPackagesHaikuPlumbingTestFixturesProjectsBlankProject",
+      "haiku-id": "0e366ff49e89"
+    },
+    children: [
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          source: "designs/comet.sketch.contents/slices/Group.svg",
+          "haiku-title": "Group",
+          "haiku-id": "364b522f7643"
+        },
+        children: [
+          {
+            elementName: "title",
+            attributes: { "haiku-id": "855ed79c7bda" },
+            children: ["Group"]
+          },
+          {
+            elementName: "desc",
+            attributes: { "haiku-id": "4d7d90c5f44a" },
+            children: ["Created with sketchtool."]
+          },
+          {
+            elementName: "defs",
+            attributes: { "haiku-id": "c341a8974b40" },
+            children: [
+              {
+                elementName: "linearGradient",
+                attributes: {
+                  id: "linearGradient-1-8ecf01",
+                  "haiku-id": "f8ff3c173459"
+                },
+                children: [
+                  {
+                    elementName: "stop",
+                    attributes: { "haiku-id": "289095f7d802" },
+                    children: []
+                  },
+                  {
+                    elementName: "stop",
+                    attributes: { "haiku-id": "898271de5df3" },
+                    children: []
+                  },
+                  {
+                    elementName: "stop",
+                    attributes: { "haiku-id": "c61620123879" },
+                    children: []
+                  }
+                ]
+              },
+              {
+                elementName: "filter",
+                attributes: {
+                  id: "filter-2-8ecf01",
+                  "haiku-id": "e39355aee3d3"
+                },
+                children: [
+                  {
+                    elementName: "feGaussianBlur",
+                    attributes: { "haiku-id": "8d762501b27a" },
+                    children: []
+                  }
+                ]
+              },
+              {
+                elementName: "linearGradient",
+                attributes: {
+                  id: "linearGradient-3-8ecf01",
+                  "haiku-id": "3d342fe86b5d"
+                },
+                children: [
+                  {
+                    elementName: "stop",
+                    attributes: { "haiku-id": "232afdd53506" },
+                    children: []
+                  },
+                  {
+                    elementName: "stop",
+                    attributes: { "haiku-id": "e6ffc1dd8570" },
+                    children: []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            elementName: "g",
+            attributes: { id: "Page-1", "haiku-id": "708ddb7f4e0b" },
+            children: [
+              {
+                elementName: "g",
+                attributes: { id: "Group", "haiku-id": "cb64161468a9" },
+                children: [
+                  {
+                    elementName: "path",
+                    attributes: {
+                      id: "Combined-Shape-Copy",
+                      "haiku-id": "21aff5934d8b"
+                    },
+                    children: []
+                  },
+                  {
+                    elementName: "ellipse",
+                    attributes: { id: "Oval-2", "haiku-id": "f18f63f54bb9" },
+                    children: []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+};
