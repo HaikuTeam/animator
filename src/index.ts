b2e45e3280dc692ed39c5a23a90cdfc9d089de1e
Matthew Trost <mrtrost@gmail.com>
2017-09-21 14:12:12 -0400

    chore: Player bump

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { client } from "haiku-sdk-client"
 let dedent = require("dedent")
 
 const banner = dedent`
-  Haiku CLI (version 2.3.4)
+  Haiku CLI (version 2.3.5)
 
   Usage:
     haiku <command> [flags]
