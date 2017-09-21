f1d46fd382ee7f13e877a57e6818643d7438eecc
Roberto Dip <dip.jesusr@gmail.com>
2017-09-21 08:47:27 -0300

    Add a checkmark haiku on the congrats step

--- packages/haiku-creator/react/components/Tour/Steps/AnimatorNotice.js
+++ packages/haiku-creator/react/components/Tour/Steps/AnimatorNotice.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import CheckTutorial from '@haiku/zack2-checktutorial/react'
 
 export default function ({ styles }) {
   return (
     <div>
+      <CheckTutorial haikuOptions={{loop: true}} />
       <h2 style={styles.heading}>Congratulations!</h2>
       <p style={styles.text}>You're now an animator.</p>
     </div>
