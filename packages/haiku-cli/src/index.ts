239993b7b273f8addd940b699fd9d09c760de3cf
Matthew Trost <mrtrost@gmail.com>
2017-09-21 08:39:52 -0400

    chore: Bump version to 2.3.4

--- packages/haiku-cli/src/index.ts
+++ packages/haiku-cli/src/index.ts
@@ -20,7 +20,7 @@ import { client } from "haiku-sdk-client"
 let dedent = require("dedent")
 
 const banner = dedent`
-  Haiku CLI (version 2.3.2)
+  Haiku CLI (version 2.3.4)
 
   Usage:
     haiku <command> [flags]
