bf18f0af97fc02e870b2d541999538f1f1d3c724
Matthew Trost <mrtrost@gmail.com>
2017-09-24 09:03:13 -0400

    tweak: compile-all script needed env set

--- scripts/compile-all.js
+++ scripts/compile-all.js
@@ -1,6 +1,11 @@
 var cp = require('child_process')
 var allPackages = require('./helpers/allPackages')()
 
+if (!process.env.NODE_ENV) {
+  // babel-cli requires this to be set for reasons I don't know
+  process.env.NODE_ENV = 'development'
+}
+
 allPackages.forEach((pack) => {
   if (pack.pkg && pack.pkg.scripts) {
     if (pack.pkg.scripts.compile) {
