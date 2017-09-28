71e344822e0b26cdc2e8cfe714acf363a2a4d4d9
Matthew Trost <mrtrost@gmail.com>
2017-09-28 10:28:36 -0400

    tweak: Optimize yarn-upgrade script

--- scripts/yarn-upgrade.js
+++ scripts/yarn-upgrade.js
@@ -1,5 +1,6 @@
 var lodash = require('lodash')
 var cp = require('child_process')
+var argv = require('yargs').argv
 var log = require('./helpers/log')
 var allPackages = require('./helpers/allPackages')()
 
@@ -11,16 +12,28 @@ var DEP_TYPES = [
 ]
 
 lodash.forEach(allPackages, function (pack) {
-  log.log('yarn upgrade for ' + pack.name)
+  // If one specific package has been mentioned, only upgrade it
+  if (argv.package && argv.package !== pack.name) {
+    return void (0)
+  }
+
+  var deps = []
 
   DEP_TYPES.forEach((type) => {
     if (pack.pkg && pack.pkg[type]) {
       lodash.forEach(pack.pkg[type], (val, key) => {
         var haiku = key.slice(0, 6)
         if (haiku === 'haiku-' || haiku === '@haiku') {
-          cp.execSync(`yarn upgrade ${key} --latest --ignore-engines --non-interactive --mutex file:/tmp/.yarn-mutex --network-concurrency 1`, { cwd: pack.abspath, stdio: 'inherit' })
+          deps.push(key)
         }
       })
     }
   })
+
+  if (deps.length > 0) {
+    log.log('yarn upgrade for ' + pack.name + ':')
+    var cmd = `yarn upgrade ${deps.join(' ')} --latest --ignore-engines --non-interactive --mutex file:/tmp/.yarn-mutex --network-concurrency 1`
+    log.log(cmd)
+    cp.execSync(cmd, { cwd: pack.abspath, stdio: 'inherit' })
+  }  
 })
