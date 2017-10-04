0ae3a42787fe0e6c048914df011a4546a0a3daca
Matthew Trost <mrtrost@gmail.com>
2017-10-04 15:40:03 -0400

    tweak: Semver bump

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { client } from "haiku-sdk-client"
 let dedent = require("dedent")
 
 const banner = dedent`
-  Haiku CLI (version 2.3.7)
+  Haiku CLI (version 2.3.8)
 
   Usage:
     haiku <command> [flags]
