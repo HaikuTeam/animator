a0ea010caa19455624b7f98b060ff689c7addc9f
Matthew Trost <mrtrost@gmail.com>
2017-10-06 10:11:10 -0400

    wip: Command to upgrade player in a list of projects (incomplete)

---
+++ test/upgrade-player.test.ts
@@ -0,0 +1,11 @@
+// import * as tape from 'tape'
+// import * as cp from 'child_process'
+// import * as path from 'path'
+// const CWD = path.join(__dirname)
+// const CLI = path.join(__dirname, '..', 'lib', 'index.js')
+
+// tape('upgrade-player', (t) => {
+//   t.plan(1)
+//   cp.execSync(`node ${JSON.stringify(CLI)} upgrade-player Interaction5`, { cwd: CWD, stdio: 'inherit' })
+//   t.ok(true)
+// })
