a803f17b9ea2cbcae795bde886dcb52fea02fad5
Matthew Trost <mrtrost@gmail.com>
2017-10-12 15:00:42 -0400

    auto: Semver update

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { argv } from "yargs"
 const dedent = require("dedent")
 
 const banner = dedent`
-  Haiku CLI (version 2.3.10)
+  Haiku CLI (version 2.3.11)
 
   Usage:
     haiku <command> [flags]
