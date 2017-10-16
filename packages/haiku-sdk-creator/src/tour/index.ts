44ed594f34d6e474ac8aab5a30c83f27e222d579
Roberto Dip <dip.jesusr@gmail.com>
2017-10-16 11:22:23 -0300

    Remove unused methods from the tour handler

--- packages/haiku-sdk-creator/src/tour/index.ts
+++ packages/haiku-sdk-creator/src/tour/index.ts
@@ -2,7 +2,6 @@ export interface Tour {
     next(): MaybeAsync<void>
     start(force?: boolean): MaybeAsync<void>
     finish(createFile?: boolean): MaybeAsync<void>
-    getCurrentStep(): MaybeAsync<number>
     notifyScreenResize(): MaybeAsync<void>
     receiveElementCoordinates(webview: string, position: ClientBoundingRect): MaybeAsync<void>
     receiveWebviewCoordinates(webview: string, coordinates: ClientBoundingRect): MaybeAsync<void>
