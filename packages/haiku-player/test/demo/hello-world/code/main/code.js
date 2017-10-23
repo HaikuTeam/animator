14cee5cdf8759762d0d19ca8464eabab2f8b7383
Matthew Trost <mrtrost@gmail.com>
2017-10-23 11:22:30 -0400

    tweak: README brush-up

--- packages/haiku-player/test/demo/hello-world/code/main/code.js
+++ packages/haiku-player/test/demo/hello-world/code/main/code.js
@@ -1,6 +1,67 @@
+// module.exports = {
+//   // timelines: {},
+//   // eventHandlers: {},
+//   // states: {},
+//   template: '<div>Hello World!</div>'
+// }
+
+// module.exports = {
+//   timelines: {
+//     Default: {
+//       "#box": {
+//         "style.width": { 0: { value: "100px" }},
+//         "style.height": { 0: { value: "100px" }},
+//         "style.backgroundColor": { 0: { value: "red" }},
+//         "rotation.z": {
+//           0: { value: 0, curve: "linear" },
+//           1000: { value: 3.14159 },
+//         },
+//       },
+//     },
+//   },
+//   template: `
+//     <div id="box"></div>
+//   `,
+// }
+
 module.exports = {
-  timelines: {},
-  eventHandlers: {},
-  states: {},
-  template: '<div>Hello World!</div>'
+  options: {
+    autoplay: false,
+  },
+  states: {
+    clicks: {
+      value: 0,
+    },
+  },
+  eventHandlers: {
+    "#box": {
+      "click": {
+        handler: function () {
+          this.state.clicks += 1
+          this.getTimeline("Default").play()
+        }
+      },
+    },
+  },
+  timelines: {
+    Default: {
+      "#box": {
+        "content": { 0: { 
+          value: function (clicks) {
+            return clicks + ""
+          },
+        }},
+        "style.width": { 0: { value: "100px" }},
+        "style.height": { 0: { value: "100px" }},
+        "style.backgroundColor": { 0: { value: "red" }},
+        "rotation.z": {
+          0: { value: 0, curve: "linear" },
+          1000: { value: 3.14159 },
+        },
+      },
+    },
+  },
+  template: `
+    <div id="box"></div>
+  `,
 }
