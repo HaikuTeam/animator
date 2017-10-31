bf2ce2557f9f52264f41efbf9eac8b01361cae12
Sasha Joseph <sasha.joseph@gmail.com>
2017-10-31 16:27:49 -0700

    fix: re-mount the ActiveComponent after extracting reified bytecode and before mutating it during Bodymovin export.

--- packages/haiku-plumbing/src/envoy/handleExporterChannel.js
+++ packages/haiku-plumbing/src/envoy/handleExporterChannel.js
@@ -5,8 +5,10 @@ import { handleExporterSaveRequest } from 'haiku-formats/lib/exporters'
 
 export default (exporterChannel, activeComponent) => {
   exporterChannel.on(`${EXPORTER_CHANNEL}:save`, (request) => {
-    const contents = handleExporterSaveRequest(
-      request, activeComponent.fetchActiveBytecodeFile().getReifiedBytecode())
+    const bytecodeSnapshot = activeComponent.fetchActiveBytecodeFile().getReifiedBytecode()
+    // Re-mount the active component so mutations to the bytecode snapshot don't trickle into the project.
+    activeComponent.mountApplication()
+    const contents = handleExporterSaveRequest(request, bytecodeSnapshot)
     fs.writeFile(request.filename, contents, (err) => {
       if (err) {
         throw err
