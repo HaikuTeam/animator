441939f1d2b719fabf35f4a2ed5ec3e3acee1366
Roberto Dip <dip.jesusr@gmail.com>
2017-11-01 11:53:30 -0300

    Rename crashReport => carbonite

--- packages/haiku-serialization/src/utils/carbonite.js
+++ packages/haiku-serialization/src/utils/carbonite.js
@@ -1,10 +1,10 @@
-// const {remote} = require('electron')
 const {exec} = require('child_process')
 const path = require('path')
 const fs = require('fs')
 const os = require('os')
 const {inkstone} = require('haiku-sdk-inkstone')
 const {client} = require('haiku-sdk-client')
+const {Experiment, experimentIsEnabled} = require('haiku-common/lib/experiments')
 const {
   HOMEDIR_PATH,
   HOMEDIR_PROJECTS_PATH,
@@ -12,30 +12,11 @@ const {
   HOMEDIR_CRASH_REPORTS_PATH
 } = require('./HaikuHomeDir')
 
-function getCurrentOrganizationName() {
-  return new Promise((resolve, reject) => {
-    var authToken = sdkClient.config.getAuthToken()
-    return inkstone.organization.list(
-      authToken,
-      (orgErr, orgsArray, orgHttpResp) => {
-        if (orgErr) return reject(new Error('Organization error'))
-        if (orgHttpResp.statusCode === 401)
-          return reject(new Error('Unauthorized organization'))
-        if (orgHttpResp.statusCode > 299)
-          return reject(
-            new Error(`Error status code: ${orgHttpResp.statusCode}`)
-          )
-        if (!orgsArray || orgsArray.length < 1)
-          return reject(new Error('No organization found'))
-
-        resolve(orgsArray[0].Name)
-      }
-    )
-  })
-}
-
 function _generateScreenshot(path) {
   return new Promise((resolve, reject) => {
+  if(true) resolve()
+
+  const {remote} = require('electron')
     remote.getCurrentWindow().capturePage(function(buf) {
       fs.writeFile(path, buf.toPng(), err => {
         err ? reject(err) : resolve()
@@ -92,26 +73,46 @@ function _cleanup(files) {
   })
 }
 
-module.exports = function crashReport(orgName, projectName) {
-  console.log("@!@#%!@#%@!#%!@#%!@#%a", orgName, projectName)
+function crashReport(orgName, projectName) {
   if (!orgName || !projectName) return
   const timestamp = new Date().getTime()
   const projectPath = path.join(HOMEDIR_PROJECTS_PATH, orgName, projectName)
   const screenshotPath = path.join(HOMEDIR_PATH, `screenshot-${timestamp}.png`)
   const zipName = `${projectName}-${timestamp}.zip`
   const zipPath = path.join(HOMEDIR_CRASH_REPORTS_PATH, zipName)
+  const userId = client.config.getUserId()
+  const AWS3Server = "http://support.haiku.ai.s3-us-west-2.amazonaws.com"
 
-  console.log('===', client.config.getUserId())
-
-  // _generateScreenshot(screenshotPath)
-    // .then(() =>
+  _generateScreenshot(screenshotPath)
+    .then(() =>
       _zipProjectFolders({
         destination: zipPath,
         sources: [projectPath, HOMEDIR_LOGS_PATH]
       })
-    // )
-    // .then(() => _getPreSignedURL(zipName))
-    // .then(AWS3URL => _upload(AWS3URL, zipPath))
+    )
+    .then(() => _getPreSignedURL(zipName))
+    .then(AWS3URL => _upload(AWS3URL, zipPath))
     .then(_cleanup([screenshotPath]))
-    .catch(err => console.log(err))
+    .catch((error) => console.log(error))
+
+  return `${AWS3Server}/${orgName}/${userId}/${zipName}`
+}
+
+function sentryCallback(data) {
+  if (data && data.extra && data.extra.organizationName && data.extra.projectName) {
+    const {organizationName, projectName} = data.extra
+    data.extra.carbonite = crashReport(organizationName, projectName)
+    data.extra.experiments = {}
+    for (const [key, value] of Object.entries(Experiment)) {
+      data.extra.experiments[key] = experimentIsEnabled(value)
+    }
+    return data
+  } else {
+    return data;
+  }
+}
+
+module.exports = {
+  crashReport,
+  sentryCallback
 }
