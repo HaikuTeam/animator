0220cc33e23907a61e4913e5afd83c39b25b07e9
Roberto Dip <dip.jesusr@gmail.com>
2017-11-06 12:51:17 -0300

    Add constants for different interaction types

---
+++ src/interaction-modes/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Interaction mode types.
+ * @namespace {object}
+ */
+export namespace InteractionMode {
+  export const Edit = {type: 'edit'} // Default mode, all is editable
+  export const Live = {type: 'live'} // Preview mode
+}
