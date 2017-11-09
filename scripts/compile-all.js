214224ce9c1318b2cac01e0882cd840f31a353d2
Matthew Trost <mrtrost@gmail.com>
2017-11-09 08:10:41 -0500

    tweak: Remove log

--- scripts/compile-all.js
+++ scripts/compile-all.js
@@ -9,7 +9,6 @@ if (!process.env.NODE_ENV) {
 allPackages.forEach((pack) => {
   if (pack.pkg && pack.pkg.scripts) {
     if (pack.pkg.scripts.compile) {
-      console.log(pack.name)
       cp.execSync('yarn run compile', { cwd: pack.abspath, stdio: 'inherit' })
     } else if (pack.pkg.scripts.transpile) {
       cp.execSync('yarn run transpile', { cwd: pack.abspath, stdio: 'inherit' })
