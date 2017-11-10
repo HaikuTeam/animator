e59f8b6456d5860a4ebd5f93ec5014787c587a6b
Sasha Joseph <sasha.joseph@gmail.com>
2017-11-10 12:20:13 -0800

    feat: Simplifies experiments and releases.

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -2,11 +2,20 @@
  * Runtime environment.
  * @enum {string}
  */
-export enum Environment {
+export const enum Environment {
   Test = 'test',
   Development = 'development',
   Staging = 'staging',
   Production = 'production',
+}
+
+/**
+ * Runtime environment type.
+ * @enum {string}
+ */
+export const enum EnvironmentType {
+  Development = 'development',
+  Release = 'release',
 }
 
 /**
@@ -15,3 +24,13 @@ export enum Environment {
  * If no environment is provided as an environment variable, assume production.
  */
 export const getEnvironment = () => global.process.env.NODE_ENV || Environment.Production;
+
+/**
+ * Gets the environment type.
+ */
+export const getEnvironmentType = () => {
+  const environment = getEnvironment();
+  return (environment === Environment.Staging || environment === Environment.Production)
+    ? EnvironmentType.Release
+    : EnvironmentType.Development;
+};
