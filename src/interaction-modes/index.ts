8668a846421d59888ad62b22dc55639fe21dddb3
Roberto Dip <dip.jesusr@gmail.com>
2017-11-12 17:29:04 -0300

    Move the interaction mode helpers to the player

--- src/interaction-modes/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Interaction mode types.
- * @namespace {object}
- */
-export namespace InteractionMode {
-  export const EDIT = {type: 'edit'}; // Default mode, all is editable
-  export const LIVE = {type: 'live'}; // Preview mode
-}
