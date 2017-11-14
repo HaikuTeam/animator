976ebe3e83135f3b1863be5faf68e60affcea41a
Matthew Trost <mrtrost@gmail.com>
2017-11-14 12:28:13 -0500

    tweak: This seems to let the peer dep install correctly, maybe

--- scripts/helpers/yarnInstall.js
+++ scripts/helpers/yarnInstall.js
@@ -9,8 +9,7 @@ module.exports = function yarnInstall (pack, cb) {
   // TODO: Why does asynchronous execution crash a Macbook Pro?
   if (pack.name === 'haiku-player') {
     cp.execSync(
-      `yarn add react@15.4.2 --peer --mutex file:/tmp/.yarn_mono_lock --cache-folder="/tmp/.yarn_mono_cache" --ignore-engines \
-          --frozen-lockfile --non-interactive`,
+      `yarn add react@15.4.2 --peer --mutex file:/tmp/.yarn_mono_lock --cache-folder="/tmp/.yarn_mono_cache" --ignore-engines --non-interactive`,
       lodash.merge(EXEC_OPTIONS, { cwd: pack.abspath, stdio: 'inherit' })
     )
   }
