bd2cd3bfa72beea9cf7e8d59df1734cc7af865df
Matthew Trost <mrtrost@gmail.com>
2017-11-14 10:39:59 -0500

    tweak: If installing in player, make sure the react peer dep is installed

--- scripts/helpers/yarnInstall.js
+++ scripts/helpers/yarnInstall.js
@@ -7,6 +7,13 @@ var EXEC_OPTIONS = {
 
 module.exports = function yarnInstall (pack, cb) {
   // TODO: Why does asynchronous execution crash a Macbook Pro?
+  if (pack.name === 'haiku-player') {
+    cp.execSync(
+      `yarn add react@15.4.2 --peer --mutex file:/tmp/.yarn_mono_lock --cache-folder="/tmp/.yarn_mono_cache" --ignore-engines \
+          --frozen-lockfile --non-interactive`,
+      lodash.merge(EXEC_OPTIONS, { cwd: pack.abspath, stdio: 'inherit' })
+    )
+  }
   cp.execSync(
     `yarn install --mutex file:/tmp/.yarn_mono_lock --cache-folder="/tmp/.yarn_mono_cache" --ignore-engines \
         --frozen-lockfile --non-interactive`,
