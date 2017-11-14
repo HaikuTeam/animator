67b823457b0fe60c6a0da8baee1ae2c8906f6e22
Matthew Trost <mrtrost@gmail.com>
2017-11-14 10:29:54 -0500

    chore: Update push script to install to avoid yarn.lock issues later

--- scripts/push.js
+++ scripts/push.js
@@ -4,7 +4,7 @@ const argv = require('yargs').argv
 const ROOT = path.join(__dirname, '..')
 
 // pull standalone remotes
-if (!argv['no-remote'] && !argv['no-pull']) {
+if (!argv['no-pull']) {
   cp.execSync(`node ./scripts/git-subtree-pull.js --package=haiku-player`, { cwd: ROOT, stdio: 'inherit' })
   cp.execSync(`node ./scripts/git-subtree-pull.js --package=haiku-cli`, { cwd: ROOT, stdio: 'inherit' })
   cp.execSync(`node ./scripts/git-subtree-pull.js --package=haiku-sdk-client`, { cwd: ROOT, stdio: 'inherit' })
@@ -33,22 +33,22 @@ cp.execSync(`git add --all .`, { cwd: ROOT, stdio: 'inherit' })
 cp.execSync(`git commit -m "auto: Recompile libs"`, { cwd: ROOT, stdio: 'inherit' })
 
 // publish packages to npm registry and cdn
-if (!argv['no-remote']) {
-  cp.execSync(`node ./scripts/upload-cdn-player.js`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`node ./scripts/publish-package.js --package=haiku-player`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`node ./scripts/publish-package.js --package=haiku-sdk-inkstone`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`node ./scripts/publish-package.js --package=haiku-sdk-client`, { cwd: ROOT, stdio: 'inherit' })
-  // update the cli's yarn.lock file appropriately
-  cp.execSync(`yarn install`, { cwd: path.join(ROOT, 'packages', 'haiku-cli'), stdio: 'inherit' })
-  cp.execSync(`git add --all .`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`git commit -m "auto: Reinstall"`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`node ./scripts/publish-package.js --package=haiku-cli`, { cwd: ROOT, stdio: 'inherit' })
-}
+cp.execSync(`node ./scripts/upload-cdn-player.js`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`node ./scripts/publish-package.js --package=haiku-player`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`node ./scripts/publish-package.js --package=haiku-sdk-inkstone`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`node ./scripts/publish-package.js --package=haiku-sdk-client`, { cwd: ROOT, stdio: 'inherit' })
+
+// update dependants' yarn.lock files appropriately
+cp.execSync(`yarn install`, { cwd: path.join(ROOT, 'packages', 'haiku-cli'), stdio: 'inherit' })
+cp.execSync(`yarn install`, { cwd: path.join(ROOT, 'packages', 'haiku-serialization'), stdio: 'inherit' })
+cp.execSync(`yarn install`, { cwd: path.join(ROOT, 'packages', 'haiku-creator'), stdio: 'inherit' })
+cp.execSync(`yarn install`, { cwd: path.join(ROOT, 'packages', 'haiku-plumbing'), stdio: 'inherit' })
+cp.execSync(`git add --all .`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`git commit -m "auto: Reinstall"`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`node ./scripts/publish-package.js --package=haiku-cli`, { cwd: ROOT, stdio: 'inherit' })
 
 // push standalone remotes
-if (!argv['no-remote']) {
-  cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-player`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-cli`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-sdk-client`, { cwd: ROOT, stdio: 'inherit' })
-  cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-sdk-inkstone`, { cwd: ROOT, stdio: 'inherit' })
-}
+cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-player`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-cli`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-sdk-client`, { cwd: ROOT, stdio: 'inherit' })
+cp.execSync(`node ./scripts/git-subtree-push.js --package=haiku-sdk-inkstone`, { cwd: ROOT, stdio: 'inherit' })
