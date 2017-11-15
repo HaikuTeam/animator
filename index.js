71678360b2c35d0b64bb50472ce2961fad3131c0
Matthew Trost <mrtrost@gmail.com>
2017-11-15 09:23:26 -0500

    fix: Fix CLI/SDK paths so haiku command works

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ process.env.HAIKU_INTERPRETER_URL_MODE = 'distro'
 if (process.env.HAIKU_APP_LAUNCH_CLI === '1') {
   process.env.HAIKU_APP_SKIP_LOG = '1'
   require('./config')
-  require('./source/plumbing/node_modules/haiku-cli')
+  require('./source/plumbing/node_modules/@haiku/cli')
 } else {
   require('./config')
   require('./source/plumbing/HaikuHelper')
