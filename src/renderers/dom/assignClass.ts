2b48d6aa5d59d8516af6e6481e06222e16183676
Sasha Joseph <sasha.joseph@gmail.com>
2017-12-04 12:26:40 -0700

    fix: use setAttribute() during DOM render instead of setting className directly.

--- src/renderers/dom/assignClass.ts
+++ src/renderers/dom/assignClass.ts
@@ -4,7 +4,7 @@
 
 export default function assignClass(domElement, className) {
   if (domElement.className !== className) {
-    domElement.className = className;
+    domElement.setAttribute('class', className);
   }
   return domElement;
 }
