012d1149bd0b482eedc93f9d3bbbb2501f2a592d
Matthew Trost <mrtrost@gmail.com>
2017-12-04 14:19:48 -0500

    Squashed 'packages/haiku-player/' changes from 6deedbc8..a3b6c1ac
    a3b6c1ac auto: Bumps semver.
    aa31d24a tweak: Add failing test showing expectation for scale outputs
    
    git-subtree-dir: packages/haiku-player
    git-subtree-split: a3b6c1ac71d8964aed4b5486a5acd50d8bd1e8b1

--- test/unit/layout.parseCssTransformString.test.js
+++ test/unit/layout.parseCssTransformString.test.js
@@ -47,7 +47,27 @@ test('layout.parseCssTransformString', function (t) {
         'scale.y': 0,
         'scale.z': 0
       }
-    ]
+    ],
+    [
+      'translate(33.000000, 92.000000) rotate(10.000000) translate(-33.000000, -92.000000) translate(-2.000000, 1.000000)',
+      {
+        "translation.x": 14.33,
+        "translation.y": -3.7,
+        "rotation.z": 0.17
+      }
+    ],
+    [
+      'translate(43.270256, 92.695192) scale(-1, 1) rotate(10.000000) translate(-43.270256, -92.695192) translate(8.270256, 1.695192)',
+      {
+        "translation.x": 61.94,
+        "translation.y": -3,
+        "rotation.y": 3.14,
+        "rotation.z": 2.97,
+        "scale.x": -1,
+        "scale.y": 1,
+        "scale.z": 1
+      }
+    ],
   ]
 
   t.plan(data.length)
