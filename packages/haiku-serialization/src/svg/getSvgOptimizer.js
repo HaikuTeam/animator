19fd833a08fa8916c746237fd13d0d16d91f0ab9
Roberto Dip <dip.jesusr@gmail.com>
2017-12-06 10:28:57 -0300

    Remove a rebase conflict

--- packages/haiku-serialization/src/svg/getSvgOptimizer.js
+++ packages/haiku-serialization/src/svg/getSvgOptimizer.js
@@ -21,23 +21,5 @@ module.exports = () => {
     })
   }
 
-<<<<<<< HEAD
-  return singleton = new svgo({
-    full: true,
-    floatPrecision: 3,
-    plugins: [
-      'removeMetadata',
-      'removeTitle',
-      'removeDesc',
-      'removeUselessDefs',
-      'removeEmptyAttrs',
-      'removeUselessStrokeAndFill',
-      'removeNonInheritableGroupAttrs',
-      'moveElemsAttrsToGroup',
-      'collapseGroups'
-    ]
-  })
-=======
   return singleton
->>>>>>> Lint fixes
 }
