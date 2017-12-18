dcf77cfe2455529bb59192773ef36e15a48a09e4
Zachary Brown <z@charybrown.com>
2017-12-17 18:47:44 -0800

    fixes for PR

--- packages/haiku-sdk-creator/src/dal/Registry.ts
+++ packages/haiku-sdk-creator/src/dal/Registry.ts
@@ -6,6 +6,23 @@ import { client as sdkClient } from '@haiku/sdk-client';
 
 const REGISTRY_PATH = path.join(os.homedir(), '.haiku', 'registry.json');
 
+function _ensureHomeFolder() {
+  mkdirp.sync(os.homedir() + '/.haiku');
+}
+
+function _getFileContents(): any {
+  _ensureHomeFolder();
+  if (!fs.existsSync(REGISTRY_PATH)) {
+    fs.writeFileSync(REGISTRY_PATH, '{}');
+  }
+  return JSON.parse(fs.readFileSync(REGISTRY_PATH).toString());
+}
+
+function _setFileContents(contents: any) {
+  _ensureHomeFolder();
+  return fs.writeFileSync(REGISTRY_PATH, JSON.stringify(contents));
+}
+
 // Purpose:  Set and retrieve locally persisted settings.
 //           Intended to supersede (and deprecate) haiku-serialization/*/HaikuHomeDir and most of haiku-sdk-client
 // Use-case: Easily store and retrieve whether the user chooses to view their timeline in ms or frames —
@@ -16,31 +33,14 @@ export class Registry {
   // TODO: cache if this ever requires hot reads/writes
   // TODO: un-staticify if logic gets much more complex
 
-  static _ensureHomeFolder() {
-    mkdirp.sync(os.homedir() + '/.haiku');
-  }
-
-  static _getFileContents(): any {
-    Registry._ensureHomeFolder();
-    if (!fs.existsSync(REGISTRY_PATH)) {
-      fs.writeFileSync(REGISTRY_PATH, '{}');
-    }
-    return JSON.parse(fs.readFileSync(REGISTRY_PATH).toString());
-  }
-
-  static _setFileContents(contents: any) {
-    Registry._ensureHomeFolder();
-    return fs.writeFileSync(REGISTRY_PATH, JSON.stringify(contents));
-  }
-
   static setConfig(key: string, value: string) {
-    const config = Registry._getFileContents();
+    const config = _getFileContents();
     config[key] = value;
     Registry._setFileContents(config);
   }
 
   static getConfig(key: string): string {
-    const config = Registry._getFileContents();
+    const config = _getFileContents();
     return config[key];
   }
 }
