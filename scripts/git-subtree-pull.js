45add6d6d4986f40f3ec9a4a85b846e2a6fafd96
Sasha Joseph <sasha.joseph@gmail.com>
2017-12-20 12:12:54 -0800

    tweak: use unsupervised subtree pull during semver bump

--- scripts/git-subtree-pull.js
+++ scripts/git-subtree-pull.js
@@ -2,10 +2,12 @@ const cp = require('child_process')
 const argv = require('yargs').argv
 const path = require('path')
 const log = require('./helpers/log')
-const ROOT = path.join(__dirname, '..')
+const nowVersion = require('./helpers/nowVersion')
 
+const ROOT = path.join(__dirname, '..')
 const branch = argv.branch || 'master'
 const packageName = argv.package
+const semver = nowVersion()
 
 if (!packageName) {
   throw new Error('a --package argument is required')
@@ -19,7 +21,8 @@ try {
   // Git subtree doesn't seem to like it unless you fetch changes first
   [
     `git fetch ${pack.remote} ${branch}`,
-    `git subtree pull --squash --prefix packages/${pack.name} ${pack.remote} ${branch}`
+    `git subtree pull --squash --prefix packages/${pack.name} ${pack.remote} ${branch} \
+     -m 'auto: subtree pull for ${packageName} at ${semver}'`
   ].forEach((cmd) => {
     log.log(cmd)
     cp.execSync(cmd, { cwd: ROOT, stdio: 'inherit' })
