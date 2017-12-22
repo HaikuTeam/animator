f71981557ba4af1c7314e2335e2642a7d533367c
Matthew Trost <mrtrost@gmail.com>
2017-12-22 11:42:30 -0500

    tweak: Prevent transmission of time updates causing stage flicker bugs during reloads

--- packages/haiku-serialization/src/bll/ActiveComponent.js
+++ packages/haiku-serialization/src/bll/ActiveComponent.js
@@ -1720,7 +1720,7 @@ class ActiveComponent extends BaseModel {
           // If we don't do this here, continued edits at this time won't work properly.
           // We have to do this  __after rehydrate__ so we update all copies fo the models we've
           // just loaded into memory who have reset attributes.
-          this.setTimelineTimeValue(timelineTimeBeforeReload, false, true, true)
+          this.setTimelineTimeValue(timelineTimeBeforeReload, true, true, true)
 
           // Start the clock again, as we should now be ready to flow updated component.
           this.instancesOfHaikuPlayerComponent.forEach((instance) => {
