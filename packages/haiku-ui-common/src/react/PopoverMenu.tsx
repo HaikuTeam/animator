70b7550998cd18c024ced2fbc8a287550b2d7f54
Matthew Trost <mrtrost@gmail.com>
2017-12-30 12:07:39 -0500

    wip: Make types versions work, add React Storybook to haiku-ui-common

---
+++ packages/haiku-ui-common/src/react/PopoverMenu.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class PopoverMenu extends React.Component {
+  render () {
+  	return <span>not yet implemented</span>;
+  }
+}
---
+++ packages/haiku-ui-common/stories/react/index.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import './PopoverMenu';
+import './OtherIcons';
