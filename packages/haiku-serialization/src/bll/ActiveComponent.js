971b6d4c6d401b040a74d075558c9c1c51b6a4df
Matthew Trost <mrtrost@gmail.com>
2018-01-04 16:06:15 -0800

    fix: Ensure comms for selecting elements works stage <-> timeline

--- packages/haiku-serialization/src/bll/ActiveComponent.js
+++ packages/haiku-serialization/src/bll/ActiveComponent.js
@@ -91,9 +91,9 @@ class ActiveComponent extends BaseModel {
     Element.on('update', (element, what, metadata) => {
       if (element.component === this) {
         if (what === 'element-selected') {
-          this.handleElementSelected(element.getPrimaryKey(), metadata)
+          this.handleElementSelected(element.getComponentId(), metadata)
         } else if (what === 'element-unselected') {
-          this.handleElementUnselected(element.getPrimaryKey(), metadata)
+          this.handleElementUnselected(element.getComponentId(), metadata)
         } else if (what === 'jit-property-added' || what === 'jit-property-removed') {
           this.reload({ hardReload: true }, {}, () => {})
         }
