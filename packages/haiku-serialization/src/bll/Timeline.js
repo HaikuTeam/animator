f0b4934e5fab6b404644765b8ede3037d22d1f7e
Sasha Joseph <sasha.joseph@gmail.com>
2018-01-20 18:58:20 -0800

    fix: for Gauge doesn't show enough values on longer projects

--- packages/haiku-serialization/src/bll/Timeline.js
+++ packages/haiku-serialization/src/bll/Timeline.js
@@ -955,9 +955,9 @@ Timeline.getMillisecondModulus = function getMillisecondModulus (pxpf) {
   if (pxpf >= 15) return 50
   if (pxpf >= 10) return 100
   if (pxpf >= 5) return 200
-  if (pxpf === 4) return 250
-  if (pxpf === 3) return 500
-  if (pxpf === 2) return 1000
+  if (pxpf >= 4) return 250
+  if (pxpf >= 3) return 500
+  if (pxpf >= 2) return 1000
   return 5000
 }
 
