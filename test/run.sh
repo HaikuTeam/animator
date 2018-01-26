17407644dbf3c143ffd854055e9d8505c45e3538
Sasha Joseph <sasha.joseph@gmail.com>
2018-01-26 08:29:45 -0800

    tweak: commit passing fixtures

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,4 @@
 set -e
-yarn electron ./node_modules/.bin/tape "./test/e2e/000*.test.js" | tap-spec
-yarn electron ./node_modules/.bin/tape "./test/e2e/001*.test.js" | tap-spec
-yarn electron ./node_modules/.bin/tape "./test/e2e/002*.test.js" | tap-spec
-yarn electron ./node_modules/.bin/tape "./test/e2e/003*.test.js" | tap-spec
+for f in `find ./test/e2e -name *.test.js | sort`; do
+    yarn electron ./node_modules/.bin/tape $f | yarn tap-spec
+done
