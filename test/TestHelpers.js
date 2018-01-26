9178734c297fe2e95bd51532e157811014c5e8c0
Matthew Trost <mrtrost@gmail.com>
2018-01-26 10:34:33 -0500

    test: Further test runner improvements

--- test/TestHelpers.js
+++ test/TestHelpers.js
@@ -119,7 +119,15 @@ const wait = (secs, cb) => {
   }
 }
 
+const stripUnstableIdsFromHtml = (html) => {
+  return html.replace(/haiku-id=".{12}"/g, '')
+             .replace(/xlink:href=".+?"/g, '')
+             .replace(/id=".+?"/g, '')
+             .replace(/url\(#.+?\)/g, '')
+}
+
 module.exports = {
   e2e,
-  wait
+  wait,
+  stripUnstableIdsFromHtml
 }
