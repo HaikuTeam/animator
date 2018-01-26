125032cc93d20ad7b19eda2c634831d786bdfb73
Matthew Trost <mrtrost@gmail.com>
2018-01-26 09:58:55 -0500

    hack: Way to run all e2e tests without hanging due to Envoy issue

---
+++ test/run.sh
@@ -0,0 +1,3 @@
+yarn electron ./node_modules/.bin/tape ./test/e2e/000_undo-shadow-element.test.js | tap-spec
+yarn electron ./node_modules/.bin/tape ./test/e2e/001_move-keyframes-rwod.test.js | tap-spec
+yarn electron ./node_modules/.bin/tape ./test/e2e/002_delete-first-polygon.test.js | tap-spec
