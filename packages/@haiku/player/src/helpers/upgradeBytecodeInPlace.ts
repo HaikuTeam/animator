911c115f072dd62d628262ab3ead738d64076f22
Matthew Trost <mrtrost@gmail.com>
2018-01-28 10:56:13 -0500

    fix: Convert legacy backgroundColor to style.backgroundColor now that we have JIT

--- packages/@haiku/player/src/helpers/upgradeBytecodeInPlace.ts
+++ packages/@haiku/player/src/helpers/upgradeBytecodeInPlace.ts
@@ -20,7 +20,6 @@ export default function upgradeBytecodeInPlace(bytecode, options) {
 
   // Convert the properties array to the states dictionary
   if (bytecode.properties) {
-    // console.info('[haiku player] auto-upgrading code properties array to states object (2.1.14+)')
     const properties = bytecode.properties;
     delete bytecode.properties;
     for (let i = 0; i < properties.length; i++) {
@@ -51,7 +50,6 @@ export default function upgradeBytecodeInPlace(bytecode, options) {
   // Convert the eventHandlers array into a dictionary
   // [{selector:'foo',name:'onclick',handler:function}] => {'foo':{'onclick':{handler:function}}}
   if (Array.isArray(bytecode.eventHandlers)) {
-    // console.info('[haiku player] auto-upgrading code event handlers to object format (2.1.14+)')
     const eventHandlers = bytecode.eventHandlers;
     delete bytecode.eventHandlers;
     bytecode.eventHandlers = {};
@@ -68,15 +66,15 @@ export default function upgradeBytecodeInPlace(bytecode, options) {
 
   // Convert a string template into our internal object format
   if (typeof bytecode.template === STRING_TYPE) {
-    // console.info('[haiku player] auto-upgrading template string to object format (2.0.0+)')
     bytecode.template = xmlToMana(bytecode.template);
   }
 
   // If specified, make sure that internal URL references, e.g. url(#my-filter), are unique
   // per each component instance, otherwise we will get filter collisions and weirdness on the page
+  const referencesToUpdate = {};
+  const alreadyUpdatedReferences = {};
+
   if (options && options.referenceUniqueness) {
-    const referencesToUpdate = {};
-    const alreadyUpdatedReferences = {};
     if (bytecode.template) {
       visitManaTree(
         '0',
@@ -96,22 +94,32 @@ export default function upgradeBytecodeInPlace(bytecode, options) {
         0,
       );
     }
-    if (bytecode.timelines) {
-      for (const timelineName in bytecode.timelines) {
-        for (const selector in bytecode.timelines[timelineName]) {
-          for (const propertyName in bytecode.timelines[timelineName][selector]) {
-            // Don't proceed if we aren't dealing with a filter attribute
-            if (propertyName !== 'filter') {
-              continue;
-            }
-            for (const keyframeMs in bytecode.timelines[timelineName][selector][propertyName]) {
-              const keyframeDesc = bytecode.timelines[timelineName][selector][propertyName][keyframeMs];
-              if (keyframeDesc && referencesToUpdate[keyframeDesc.value]) {
-                // console.info(
-                //   '[haiku player] changing filter url reference ' + keyframeDesc.value + ' to ' +
-                //   referencesToUpdate[keyframeDesc.value]);
-                keyframeDesc.value = referencesToUpdate[keyframeDesc.value];
-              }
+  }
+
+  if (bytecode.timelines) {
+    for (const timelineName in bytecode.timelines) {
+      for (const selector in bytecode.timelines[timelineName]) {
+        // Legacy backgroundColor was a root prop; in newer versions it's style.backgroundColor.
+        // We only want to update this if the user *hasn't* explicitly set style.backroundColor.
+        if (
+          bytecode.timelines[timelineName][selector]['backgroundColor'] &&
+          !bytecode.timelines[timelineName][selector]['style.backgroundColor']
+        ) {
+          bytecode.timelines[timelineName][selector]['style.backgroundColor'] =
+            bytecode.timelines[timelineName][selector]['backgroundColor'];
+
+          delete bytecode.timelines[timelineName][selector]['backgroundColor'];
+          continue;
+        }
+
+        // If we're a filter attribute, update our references per those to whom uniqueness was added above/
+        // This appends a "*-abc123" string to the filter to avoid collisions when multiple same components
+        // are mounted in a single web page
+        if (bytecode.timelines[timelineName][selector]['filter']) {
+          for (const keyframeMs in bytecode.timelines[timelineName][selector]['filter']) {
+            const keyframeDesc = bytecode.timelines[timelineName][selector]['filter'][keyframeMs];
+            if (keyframeDesc && referencesToUpdate[keyframeDesc.value]) {
+              keyframeDesc.value = referencesToUpdate[keyframeDesc.value];
             }
           }
         }
