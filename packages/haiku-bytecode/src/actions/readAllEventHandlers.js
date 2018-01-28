144a793c0bf93600e6fd47f70d9eb82c96fa9f68
Matthew Trost <mrtrost@gmail.com>
2018-01-28 10:54:44 -0500

    tweak: Don't call upgradeBytecodeInPlace on every action; it should happen upstream

--- packages/haiku-bytecode/src/actions/readAllEventHandlers.js
+++ packages/haiku-bytecode/src/actions/readAllEventHandlers.js
@@ -1,9 +1,4 @@
-var upgradeBytecodeInPlace = require('@haiku/player/lib/helpers/upgradeBytecodeInPlace').default
-
 module.exports = function readAllEventHandlers (bytecode) {
-  // To convert legacy event handlers array to object
-  upgradeBytecodeInPlace(bytecode)
-
   if (!bytecode.eventHandlers) {
     bytecode.eventHandlers = {}
   }
