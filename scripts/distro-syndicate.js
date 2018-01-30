cd16d8ac05e12cdeab7aa33d9bd2e7cf732b09f2
Sasha Joseph <sasha.joseph@gmail.com>
2018-01-29 18:07:39 -0800

    infra: add script for syndication with CDN invalidation

---
+++ scripts/distro-syndicate.js
@@ -0,0 +1,116 @@
+const path = require('path')
+const inquirer = require('inquirer')
+const argv = require('yargs').argv
+
+require('../config')
+const deploy = require('./deploy')
+const initializeAWSService = require('./helpers/initializeAwsService')
+const log = require('./helpers/log')
+
+const platform = global.process.env.HAIKU_RELEASE_PLATFORM
+const branch = global.process.env.HAIKU_RELEASE_BRANCH
+const version = global.process.env.HAIKU_RELEASE_VERSION
+const environment = global.process.env.HAIKU_RELEASE_ENVIRONMENT
+
+const region = deploy.deployer[environment].region
+const key = deploy.deployer[environment].key
+const secret = deploy.deployer[environment].secret
+const bucket = deploy.deployer[environment].bucket
+const distribution = deploy.deployer[environment].distribution
+
+const s3 = initializeAWSService('S3', region, key, secret)
+const cloudFront = initializeAWSService('CloudFront', region, key, secret)
+
+const latestKey = `releases/${environment}-${branch}-${platform}-latest.zip`
+
+const syndicate = (patchKey) => {
+  // Deploy the patch update to catch in-app users.
+  s3.copyObject({Bucket: bucket, CopySource: `/${bucket}/${patchKey}`, Key: patchKey.replace('-pending', '')})
+    .promise()
+    .then(() => {
+      // Deploy the Inkstone update to catch new users.
+      s3.copyObject({Bucket: bucket, CopySource: `/${bucket}/${patchKey}`, Key: latestKey})
+        .promise()
+        .then(() => {
+          cloudFront.createInvalidation({
+            DistributionId: distribution,
+            InvalidationBatch: {
+              CallerReference: Date.now().toString(),
+              Paths: {
+                Quantity: 1,
+                Items: [`/${latestKey}`]
+              }
+            }
+          })
+            .promise()
+            .then(() => {
+              log.hat('Syndication complete!')
+            })
+            .catch(() => {
+              log.err('Unable to create CloudFront invalidation!')
+              global.process.exit(1)
+            })
+        })
+        .catch(() => {
+          log.err(`Unable to syndicate latest key (${latestKey})`)
+          global.process.exit(1)
+        })
+    })
+    .catch(() => {
+      log.err(`Unable to syndicate patch key (${patchKey})`)
+      global.process.exit(1)
+    })
+}
+
+const maybeSyndicate = (data) => {
+  const potentialPatches = data.Contents.filter((key) =>
+    path.basename(key.Key).startsWith(`Haiku-${version}-${platform}`))
+  if (potentialPatches.length === 0) {
+    log.err('Unable to find pending patch key!')
+    global.process.exit(1)
+  }
+
+  if (potentialPatches.length > 1) {
+    log.err(`Found too many potential patches:\n${potentialPatches.map((obj) => obj.Key).join('\n')}`)
+    global.process.exit(1)
+  }
+
+  const patchKey = potentialPatches[0].Key
+  if (!patchKey.endsWith('-pending.zip')) {
+    log.err(`Aborting attempt to release already released patch: ${patchKey}`)
+    global.process.exit(1)
+  }
+
+  log.log(`Found patch key: ${patchKey}`)
+
+  log.warn(`WARNING: about to syndicate ${patchKey} and ${latestKey}!`)
+
+  if (argv['non-interactive']) {
+    syndicate(patchKey)
+    return
+  }
+
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'continue',
+      message: 'Continue',
+      default: false
+    }
+  ]).then((answers) => {
+    if (!answers.continue) {
+      log.log('Aborting publish by user request')
+      global.process.exit(0)
+    }
+
+    syndicate(patchKey)
+  })
+}
+
+s3.listObjects({Bucket: bucket, Prefix: `releases/${environment}/${branch}/${platform}`})
+  .promise()
+  .then(maybeSyndicate)
+  .catch(() => {
+    log.err('Unable to list candidate patch releases')
+    global.process.exit(1)
+  })
--- scripts/helpers/uploadRelease.js
+++ scripts/helpers/uploadRelease.js
@@ -13,7 +13,6 @@ function uploadRelease (region, key, secret, bucket, folder, platform, environme
   var countdown = (Math.pow(10, 13) - Date.now()) + '' // Reverse timestamp for ordering
 
   var target = path.join(folder, environment, branch, platform, countdown, version)
-  var latest = path.join(folder, [environment, branch, platform].join('-'))
 
   return fs.readdir(source, function (err, entries) {
     if (err) return cb(err)
@@ -38,30 +37,12 @@ function uploadRelease (region, key, secret, bucket, folder, platform, environme
 
       console.log('Uploading ' + build + ' as object ' + key + ' to bucket ' + bucket + '...')
 
-      return uploadObjectToS3(s3, key, stream, bucket, 'public-read', function (err) {
-        if (err) return next(err)
-
-        // The 'latest' upload variant doesn't include the -pending label since it is
-        // not used to determine syndication behavior on auto-update
-        key = [latest, 'latest' + path.extname(entry)].join('-')
-
-        // Upload with a 'pending' label so squirrel server knows not to include this in
-        // the list of candidates. This gives us an opportunity to test before syndication.
-        // The idea is that we manually remove the pending flag on S3 when we're ready.
-        key = key.replace(/.zip$/, '-pending.zip')
-
-        stream = fs.createReadStream(build)
-
-        console.log('Uploading ' + build + ' as object ' + key + ' to bucket ' + bucket + '...')
-
-        return uploadObjectToS3(s3, key, stream, bucket, 'public-read', next)
-      })
+      return uploadObjectToS3(s3, key, stream, bucket, 'public-read', next)
     }, (err) => {
       if (err) return cb(err)
 
       var urls = {
         download: `https://s3.amazonaws.com/${bucket}/${folder}/${environment}/${branch}/${platform}/${countdown}/${version}/Haiku-${version}-${platform}-pending.zip`,
-        latest: `https://s3.amazonaws.com/${bucket}/${folder}/${environment}-${branch}-${platform}-latest-pending.zip`
       }
 
       return cb(null, {
