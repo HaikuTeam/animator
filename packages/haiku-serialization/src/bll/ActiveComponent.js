73ed89b969b3726e2af0ff45c83925d1d58707b8
Matthew Trost <mrtrost@gmail.com>
2018-02-08 07:03:49 -0500

    tweak: Wait to see if the missing element arrives before crashing

--- packages/haiku-serialization/src/bll/ActiveComponent.js
+++ packages/haiku-serialization/src/bll/ActiveComponent.js
@@ -387,46 +387,70 @@ class ActiveComponent extends BaseModel {
     })
   }
 
+  selectElementWithinTime (waitTime, componentId, metadata, cb) {
+    const element = Element.findByComponentAndHaikuId(this, componentId)
+
+    // If we don't initially find the element, wait up to `waitTime` to see if it appears
+    // Race conditions with instantiate can cause this to happen
+    if (!element) {
+      if (waitTime <= 0) {
+        // In what circumstances could this happen that we would want to continue?
+        throw new Error(`Cannot select element ${componentId} in ${JSON.stringify(this.getTopLevelElementHaikuIds())}`)
+      }
+
+      return setTimeout(() => {
+        return this.selectElementWithinTime(waitTime - 250, componentId, metadata, cb)
+      }, 250)
+    }
+
+    element.select(metadata)
+
+    const row = element.getHeadingRow()
+
+    if (row) {
+      row.expand(metadata)
+    }
+
+    cb()
+  }
+
   selectElement (componentId, metadata, cb) {
     return Lock.request(Lock.LOCKS.ActiveComponentWork, (release) => {
       // Assuming the update occurs remotely, we want to unselect everything but the selected one
       Element.unselectAllElements({ component: this }, metadata)
 
-      const element = Element.findByComponentAndHaikuId(this, componentId)
-
-      // In what circumstances could this happen that we would want to continue?
-      if (!element) {
-        throw new Error(`Cannot select element ${componentId} in ${JSON.stringify(this.getTopLevelElementHaikuIds())}`)
-      }
-
-      element.select(metadata)
-
-      const row = element.getHeadingRow()
-
-      if (row) {
-        row.expand(metadata)
-      }
-
-      release()
-
-      return cb() // Must return or the plumbing action circuit never completes
+      return this.selectElementWithinTime(1000, componentId, metadata, () => {
+        release()
+        return cb() // Must return or the plumbing action circuit never completes
+      })
     })
+  }
+
+  unselectElementWithinTime (waitTime, componentId, metadata, cb) {
+    const element = Element.findByComponentAndHaikuId(this, componentId)
+
+    if (!element) {
+      if (waitTime <= 0) {
+        // Is it better to throw here, like we do with selectElement?
+        return cb()
+      }
+
+      return setTimeout(() => {
+        return this.unselectElementWithinTime(waitTime - 250, componentId, metadata, cb)
+      }, 250)
+    }
+
+    element.unselect(metadata)
+
+    return cb()
   }
 
   unselectElement (componentId, metadata, cb) {
     return Lock.request(Lock.LOCKS.ActiveComponentWork, (release) => {
-      const element = Element.findByComponentAndHaikuId(this, componentId)
-
-      if (element) {
-        element.unselect(metadata)
-      } else {
-        // In what circumstances could this happen that we would want to continue?
-        // throw new Error(`Cannot unselect element ${componentId} in ${JSON.stringify(this.getTopLevelElementHaikuIds())}`)
-      }
-
-      release()
-
-      return cb() // Must return or the plumbing action circuit never completes
+      return this.unselectElementWithinTime(1000, componentId, metadata, () => {
+        release()
+        return cb() // Must return or the plumbing action circuit never completes
+      })
     })
   }
 
