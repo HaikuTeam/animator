e5768f794a955fa2569228c45681e1c6ae228197
Matthew Trost <mrtrost@gmail.com>
2018-02-10 10:45:03 -0500

    feat: Method to enumerate projects from the home directory

--- packages/haiku-serialization/src/utils/HaikuHomeDir.js
+++ packages/haiku-serialization/src/utils/HaikuHomeDir.js
@@ -1,11 +1,13 @@
-var path = require('path')
-var os = require('os')
-var fse = require('fs-extra')
+const path = require('path')
+const os = require('os')
+const fse = require('fs-extra')
+const async = require('async')
 
-var out = {}
-var didTakeTourCache = null
+const out = {}
 
-var HOMEDIR_PATH = path.join(os.homedir(), '.haiku')
+let didTakeTourCache = null
+
+const HOMEDIR_PATH = path.join(os.homedir(), '.haiku')
 
 out.HOMEDIR_PATH = HOMEDIR_PATH
 out.HOMEDIR_AUTH_PATH = path.join(HOMEDIR_PATH, 'auth')
@@ -20,7 +22,7 @@ out.HOMEDIR_SKETCH_DIALOG_PATH = path.join(HOMEDIR_PATH, 'sketch-dialog')
 // existence of the HOMEDIR_TOUR_PATH.
 //
 // @returns Boolean
-out.didTakeTour = function () {
+out.didTakeTour = () => {
   if (didTakeTourCache === null) {
     didTakeTourCache = fse.existsSync(out.HOMEDIR_TOUR_PATH)
   }
@@ -29,7 +31,7 @@ out.didTakeTour = function () {
 }
 
 // Creates a file to store tour options
-out.createTourFile = function () {
+out.createTourFile = () => {
   return fse.ensureFileSync(out.HOMEDIR_TOUR_PATH)
 }
 
@@ -37,13 +39,65 @@ out.createTourFile = function () {
 // existence of the HOMEDIR_SKETCH_DIALOG_PATH.
 //
 // @returns Boolean
-out.didAskedForSketch = function () {
+out.didAskedForSketch = () => {
   return fse.existsSync(out.HOMEDIR_SKETCH_DIALOG_PATH)
 }
 
 // Creates a file to store tour options
-out.createSketchDialogFile = function () {
+out.createSketchDialogFile = () => {
   return fse.ensureFileSync(out.HOMEDIR_SKETCH_DIALOG_PATH)
+}
+
+function isDir (abspath) {
+  try {
+    return fse.lstatSync(abspath).isDirectory()
+  } catch (exception) {
+    console.warn(exception)
+    return false
+  }
+}
+
+out.enumerateAllProjectsByOrganization = (cb) => {
+  return fse.readdir(out.HOMEDIR_PROJECTS_PATH, (err, orgEntries) => {
+    if (err) return err
+
+    const organizations = {}
+
+    return async.each(orgEntries, (orgEntry, nextOrgEntry) => {
+      const orgAbspath = path.join(out.HOMEDIR_PROJECTS_PATH, orgEntry)
+
+      // Don't include any orgs that aren't directories
+      if (!isDir(orgAbspath)) return nextOrgEntry()
+      if (orgEntry[0] === '.') return nextOrgEntry()
+
+      organizations[orgEntry] = []
+
+      return fse.readdir(orgAbspath, (err, projEntries) => {
+        if (err) return nextOrgEntry()
+        if (!projEntries) return nextOrgEntry()
+
+        projEntries.forEach((projEntry) => {
+          const projAbspath = path.join(orgAbspath, projEntry)
+
+          // Only include visible directories that aren't backups or weird files
+          if (!isDir(projAbspath)) return
+          if (projEntry[0] === '.') return
+          if (projEntry[0] === '~') return
+          if (projEntry.match(/\.bak/)) return
+
+          organizations[orgEntry].push({
+            project: projEntry,
+            abspath: projAbspath
+          })
+        })
+
+        return nextOrgEntry()
+      })
+    }, (err) => {
+      if (err) return cb(err)
+      return cb(null, organizations)
+    })
+  })
 }
 
 module.exports = out
