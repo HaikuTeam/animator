0218ff43468504bb6a5061efe93eaa37c4fdfa76
Roberto Dip <dip.jesusr@gmail.com>
2018-02-12 17:28:58 -0300

    tweak: update tour instructions to match the new publish ui

--- packages/haiku-creator/src/react/components/Tour/Steps/Publish.js
+++ packages/haiku-creator/src/react/components/Tour/Steps/Publish.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import EmbeddedVideo from '../EmbeddedVideo'
 
-export default function ({ styles }) {
+export default function({styles}) {
   return (
     <div>
-      <h2 style={styles.heading}>Publish</h2>
+      <h2 style={styles.heading}>Publishing and Installing</h2>
       <div style={styles.text}>
         <p>
-          Publish your project to preview on the web and view embed instructions.
+          Publish your design as a production-ready component by clicking
+          Publish in the top right corner.
         </p>
         <EmbeddedVideo name={'Publish'} />
+        <p>
+          View instructions on how to install your component in Web & Mobile
+          codebases. Grab a shareable link to view your design on the web,
+          including GIF preview.
+        </p>
       </div>
     </div>
   )
