4f10b56151a1a90aabf8332748218011d0c997d6
Roberto Dip <dip.jesusr@gmail.com>
2018-02-12 14:31:36 -0300

    fix: update the copy to installing npm published projects

--- src/react/ShareModal/ShareOptions/NpmInstallable.tsx
+++ src/react/ShareModal/ShareOptions/NpmInstallable.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react'
 import * as Color from 'color'
+import * as dedent from 'dedent'
 import {PUBLISH_SHARED} from './PublishStyles'
 
 export class NpmInstallable extends React.PureComponent {
+  props
+
+  static propTypes = {
+    projectName: React.PropTypes.string,
+    userName: React.PropTypes.string,
+    organizationName: React.PropTypes.string
+  }
+
   render() {
+    const {projectName, userName, organizationName} = this.props
+
     return (
       <div style={PUBLISH_SHARED.block}>
         <div style={PUBLISH_SHARED.instructionsRow}>
@@ -27,14 +38,19 @@ export class NpmInstallable extends React.PureComponent {
           </div>
           <div style={PUBLISH_SHARED.instructionsCol2}>
             <div style={{marginTop: 19}}>
-              Run the following from your React project folder: <br />
+              Run the following from your project folder: <br />
             </div>
           </div>
         </div>
         <div style={PUBLISH_SHARED.instructionsRow}>
           <div style={PUBLISH_SHARED.instructionsCol1}>&nbsp;</div>
           <div style={PUBLISH_SHARED.instructionsCol2}>
-            <code style={PUBLISH_SHARED.code}>haiku install percy</code>
+            <code style={PUBLISH_SHARED.code}>
+              {dedent`
+                haiku init
+                npm i --save @haiku/${organizationName.toLowerCase()}-${projectName}
+              `}
+            </code>
           </div>
         </div>
 
