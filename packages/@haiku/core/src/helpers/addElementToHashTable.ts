d32ddf4b6806abbbec1baa2dfb35ce353a6b542d
Matthew Trost <mrtrost@gmail.com>
2018-02-12 11:44:45 -0500

    tweak: Don't track elements that have no id

--- packages/@haiku/core/src/helpers/addElementToHashTable.ts
+++ packages/@haiku/core/src/helpers/addElementToHashTable.ts
@@ -6,20 +6,24 @@ export default function addElementToHashTable(hash, realElement, virtualElement)
   if (virtualElement && virtualElement.attributes) {
     const flexId = virtualElement.attributes['haiku-id'] || virtualElement.attributes.id;
 
-    if (!hash[flexId]) {
-      hash[flexId] = [];
-    }
-
-    let alreadyInList = false;
-    for (let i = 0; i < hash[flexId].length; i++) {
-      const elInList = hash[flexId][i];
-      if (elInList === realElement) {
-        alreadyInList = true;
+    // Don't add if there is no id, otherwise we'll end up tracking a bunch
+    // of elements all sharing a key such as `undefined` or `null` etc.
+    if (flexId) {
+      if (!hash[flexId]) {
+        hash[flexId] = [];
       }
-    }
 
-    if (!alreadyInList) {
-      hash[flexId].push(realElement);
+      let alreadyInList = false;
+      for (let i = 0; i < hash[flexId].length; i++) {
+        const elInList = hash[flexId][i];
+        if (elInList === realElement) {
+          alreadyInList = true;
+        }
+      }
+
+      if (!alreadyInList) {
+        hash[flexId].push(realElement);
+      }
     }
   }
 }
