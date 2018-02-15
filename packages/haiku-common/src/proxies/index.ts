bba56d65810972f59b47d1bcf475d9108691676f
Roberto Dip <dip.jesusr@gmail.com>
2018-02-15 15:22:45 -0300

    wip: add a new module to haiku-common with proxy utilities

---
+++ packages/haiku-common/src/proxies/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Proxy Type.
+ * @enum {string}
+ */
+export const enum ProxyType {
+  Proxyed = 'PROXY',
+  Direct = 'DIRECT',
+}
+
+/**
+ * Canonical method to detect if a proxy string provided by electron
+ */
+export const isProxied = (proxyString: string) => {
+  return proxyString === ProxyType.Direct
+};
