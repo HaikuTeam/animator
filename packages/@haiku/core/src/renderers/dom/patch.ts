743c2f2dccd29b05626013151b7bb1786d02fbaa
Matthew Trost <mrtrost@gmail.com>
2018-02-27 17:32:15 -0500

    fix: Ensure we update the cached DOM element after a replacement; simply the cache

--- packages/@haiku/core/src/renderers/dom/patch.ts
+++ packages/@haiku/core/src/renderers/dom/patch.ts
@@ -18,18 +18,27 @@ export default function patch(topLevelDomElement, patchesDict, component) {
   for (const flexId in patchesDict) {
     const virtualElement = patchesDict[flexId];
 
-    const domElements = component._getRealElementsAtId(flexId);
+    const domElement = component._getRealElementAtId(flexId);
 
-    const nestedModuleElement = component._nestedComponentElements[flexId];
+    if (domElement) {
+      const nestedModuleElement = component._nestedComponentElements[flexId];
 
-    for (let i = 0; i < domElements.length; i++) {
-      const domElement = domElements[i];
-
-      updateElement(domElement, virtualElement, domElement.parentNode, virtualElement.__parent, component, true);
+      updateElement(
+        domElement,
+        virtualElement,
+        domElement.parentNode,
+        virtualElement.__parent,
+        component,
+        true,
+      );
 
       // If there is a nested component at this location, let it patch inside its scope
       if (nestedModuleElement && nestedModuleElement.__instance) {
-        patch(domElement, nestedModuleElement.__instance._getPrecalcedPatches(), nestedModuleElement.__instance);
+        patch(
+          domElement,
+          nestedModuleElement.__instance._getPrecalcedPatches(),
+          nestedModuleElement.__instance,
+        );
       }
     }
   }
