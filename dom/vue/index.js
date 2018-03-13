d74dcebbcfa3cfc3fec3d9072a88f9ab5ba567eb
Matthew Trost <mrtrost@gmail.com>
2018-03-13 10:38:30 -0400

    style: Add semicolons

--- dom/vue/index.js
+++ dom/vue/index.js
@@ -2,4 +2,4 @@
  * Copyright (c) Haiku 2016-2018. All rights reserved.
  */
 
-module.exports = require('./../../lib/adapters/vue-dom').default
+module.exports = require('./../../lib/adapters/vue-dom').default;
