fa0dc2519be7b86802c15876f92bc0071b608a1b
Roberto Dip <dip.jesusr@gmail.com>
2018-03-19 14:25:19 -0300

    chore: configure webpack for storybook in order to import styles from creator

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,20 @@
+const path = require('path')
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+
 module.exports = (baseConfig, env) => {
   const config = genDefaultConfig(baseConfig, env);
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: ['awesome-typescript-loader', 'webpack-conditional-loader']
   });
+  config.module.rules.push({
+    test: /\.css$/,
+    include: path.resolve(__dirname, '../../haiku-creator/public/assets/')
+  });
+  config.module.rules.push({
+    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    loader: 'url-loader?limit=100000'
+  })
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
