607164c39aa5ed148f00afa7c5ce5b1a5b6d5285
Sasha Joseph <sasha.joseph@gmail.com>
2018-03-25 22:07:55 -0700

    feat: proxy serialization capability in haiku-common

--- packages/haiku-common/src/proxies/index.ts
+++ packages/haiku-common/src/proxies/index.ts
@@ -1,8 +1,10 @@
+import {parse} from 'url';
+
 /**
  * Proxy Type.
  * @enum {string}
  */
-export const enum ProxyType {
+export enum ProxyType {
   Proxied = 'PROXY',
   Direct = 'DIRECT',
 }
@@ -12,4 +14,56 @@ export const enum ProxyType {
  */
 export const isProxied = (proxyString: string) => {
   return proxyString !== ProxyType.Direct;
+};
+
+export type ProxyDescriptor = {
+  host: string;
+  port?: number;
+  username?: string;
+  password?: string;
+  disableEncryption?: boolean;
+};
+
+/**
+ * Given a proxy descriptor, return a fully-qualified proxy URL.
+ * @param {ProxyDescriptor} descriptor
+ * @returns {string}
+ * TODO: Do we ever need to support secure proxy URLs?
+ */
+export const buildProxyUrl = (descriptor: ProxyDescriptor): string => {
+  if (!descriptor.host) {
+    return '';
+  }
+
+  const portSuffix = descriptor.port ? `:${descriptor.port}` : '';
+
+  if (descriptor.username && descriptor.password) {
+    return `http://${encodeURIComponent(descriptor.username)}:${encodeURIComponent(descriptor.password)}@` +
+      `${descriptor.host}${portSuffix}`;
+  }
+
+  return `http://${descriptor.host}${portSuffix}`;
+};
+
+/**
+ * Given a PAC-sourced URL like 'secure.megacorp.com:3128', return a ProxyDescriptor object.
+ * @param {string} url
+ * @returns {ProxyDescriptor}
+ */
+export const describeProxyFromUrl = (url?: string): ProxyDescriptor => {
+  if (!url || !isProxied(url)) {
+    return {host: ''};
+  }
+
+  const parsedUrl = parse(url.startsWith('http') ? url : `http://${url}`);
+  const proxyDescriptor = {host: parsedUrl.hostname} as ProxyDescriptor;
+  if (parsedUrl.port) {
+    proxyDescriptor.port = Number(parsedUrl.port);
+  }
+
+  if (parsedUrl.auth) {
+    [proxyDescriptor.username, proxyDescriptor.password] = parsedUrl.auth.split(':');
+  }
+
+  return proxyDescriptor;
 };
