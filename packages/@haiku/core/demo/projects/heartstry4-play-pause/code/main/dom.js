67872cfd6ab36d6071919c17f804cbdeb1a66d75
Matthew Trost <mrtrost@gmail.com>
2018-05-10 09:45:40 -0400

    chore: Add demo project to verify play/pause behavior

---
+++ packages/@haiku/core/demo/projects/heartstry4-play-pause/code/main/dom.js
@@ -0,0 +1,16 @@
+const factory = require('@haiku/core/dom')
+
+module.exports = factory(require('./code.js'), {
+  autoplay: true,
+  loop: true,
+  onHaikuComponentDidMount: function (instance) {
+    const tl = instance.getDefaultTimeline()
+
+    setTimeout(function () {
+      tl.pause()
+      setTimeout(() => {
+        tl.play()
+      }, 500)
+    }, 500)
+  }
+})
