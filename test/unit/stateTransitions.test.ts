b1bb458ef312e953508c37bfaae47d84d353908b
jonaias <jonas.jonaias@gmail.com>
2018-05-18 18:39:06 -0300

    Added StateTransitions unit test

---
+++ test/unit/stateTransitions.test.ts
@@ -0,0 +1,72 @@
+import * as tape from 'tape';
+
+import HaikuComponent from './../../src/HaikuComponent';
+import HaikuContext from './../../src/HaikuContext';
+import HaikuClock from './../../src/HaikuClock';
+import StateTransitions from './../../src/StateTransitions';
+
+
+tape('Test state transitions', (t) => {
+  t.plan(13);
+
+  const states = {var1:0, var2:5};
+  const haikuClock = new HaikuClock({}, {});
+  
+  const stateTransitions = new StateTransitions(states, haikuClock);
+
+
+  stateTransitions.createNewTransition({var1:10, var2:10}, {duration: 4000, curve: 'linear'});
+
+
+  haikuClock.setTime(0);
+  stateTransitions.tickStateTransitions();
+  t.deepEqual([states.var1,  states.var2], [0, 5], 'Simple state transition at 0ms');
+
+  haikuClock.setTime(1000);
+  stateTransitions.tickStateTransitions();
+  t.deepEqual([states.var1,  states.var2], [2.5, 6.25], 'Simple state transition at 1000ms');
+
+  haikuClock.setTime(2000);
+  stateTransitions.tickStateTransitions();
+  t.deepEqual([states.var1,  states.var2], [5, 7.5], 'Simple state transition at 2000ms');
+
+  haikuClock.setTime(4000);
+  stateTransitions.tickStateTransitions();
+  t.deepEqual([states.var1,  states.var2], [10, 10], 'Simple state transition at 4000ms');
+
+  t.is(stateTransitions.transitions.length, 0, 'Check is expired transition is deleted');
+
+
+  stateTransitions.createNewTransition({var1:5}, {duration: 1000, curve: 'linear'});
+  stateTransitions.createNewTransition({var2:0}, {duration: 2000, curve: 'linear'});
+  t.deepEqual([states.var1,  states.var2], [10, 10], 'Dual state transition at 4000ms');
+
+
+  t.is(stateTransitions.transitions.length, 2, 'Check number of transitions at 6000ms');
+
+  haikuClock.setTime(5000);
+  stateTransitions.tickStateTransitions();
+  t.deepEqual([states.var1,  states.var2], [5, 5], 'Dual state transition at 5000ms');
+
+  t.is(stateTransitions.transitions.length, 1, 'Check number of transitions at 6000ms');
+
+  haikuClock.setTime(6000);
+  stateTransitions.tickStateTransitions();
+  t.deepEqual([states.var1,  states.var2], [5, 0], 'Dual state transition at 6000ms');
+
+  t.is(stateTransitions.transitions.length, 0, 'Check number of transitions at 6000ms');
+
+
+  stateTransitions.createNewTransition({var3:5, var1:0}, {duration: 1000, curve: 'linear'});
+  haikuClock.setTime(7000);
+  stateTransitions.tickStateTransitions();
+  t.ok(!('var3' in states), 'Ignore non pre existant states');
+
+  stateTransitions.createNewTransition({var1:5}, {duration: 10000, curve: 'linear'});
+  stateTransitions.createNewTransition({var2:5}, {duration: 20000, curve: 'linear'});
+
+  haikuClock.setTime(7000);
+  stateTransitions.tickStateTransitions();
+  stateTransitions.deleteAllStateTransitions();
+  t.is(stateTransitions.transitions.length, 0, 'Delete all state transitions');  
+});
