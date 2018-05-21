6f2cb1f9e4bfbfc1500e6dd1c5ab612143d0eb1c
Matt Reyer <matthew5384@gmail.com>
2018-05-21 16:25:56 -0400

    Updated test to be conformant to new TS format.

--- packages/@haiku/core/test/unit/SVGPoints.polyPointsStringToPoints.test.ts
+++ packages/@haiku/core/test/unit/SVGPoints.polyPointsStringToPoints.test.ts
@@ -1,16 +1,16 @@
 'use strict'
 
-var test = require('tape')
+import * as tape from 'tape';
 var SVGPoints = require('./../../lib/helpers/SVGPoints').default
 
-test('SVGPoints.polyPointsStringToPoints', function (t) {
+tape('SVGPoints.polyPointsStringToPoints', (t) => {
   
-  t.plan(1)
+  t.plan(1);
   
-  var points1 = "149.2,12  49.2 209.1 101     108.2    20  ,  284  50, 28"
+  const points1 = "149.2,12  49.2 209.1 101     108.2    20  ,  284  50, 28";
   t.equal(
     JSON.stringify([[149.2, 12], [49.2, 209.1], [101, 108.2], [20, 284], [50, 28]]),
     JSON.stringify(SVGPoints.polyPointsStringToPoints(points1))
-  )
+  );
   
 })
