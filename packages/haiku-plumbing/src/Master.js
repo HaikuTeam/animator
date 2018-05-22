5b331e76b3e5b6d33611c009192ee98e41bc988a
Matthew Trost <mrtrost@gmail.com>
2018-05-22 17:55:51 -0400

    chore: Add null return to make lint fixer not do bad thing

--- packages/haiku-plumbing/src/Master.js
+++ packages/haiku-plumbing/src/Master.js
@@ -396,7 +396,7 @@ export default class Master extends EventEmitter {
             }
 
             if (relpath !== this.getActiveComponent().fetchActiveBytecodeFile().relpath) {
-              return
+              return null
             }
           })
         }
