e33661e6f26341fdb4cbf53b6e6805796284311f
Sasha Joseph <sasha.joseph@gmail.com>
2018-05-30 14:14:00 -0700

    chore: add a comment explaining why we need this postinstall script.

--- scripts/link-binaries.js
+++ scripts/link-binaries.js
@@ -5,6 +5,8 @@ const {readdirSync, ensureSymlinkSync} = require('haiku-fs-extra')
 
 const packages = require('./helpers/packages')
 
+// We want to avail ourselves of the linked binaries from haiku-testing in all projects that use it as a devDependency.
+// Because of our unique yarn workspace setup, we have to manually link these in a postinstall script. For reasons.
 const haikuTestingPackageAbsPath = packages('haiku-testing').abspath
 const binaries = readdirSync(path.join(haikuTestingPackageAbsPath, 'bin')).map(
   (binary) => path.resolve(haikuTestingPackageAbsPath, 'bin', binary)
