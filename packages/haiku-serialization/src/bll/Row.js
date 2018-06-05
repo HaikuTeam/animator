8523943e8d4e575c72b0731922714f38f8074813
Matthew Trost <mrtrost@gmail.com>
2018-06-05 07:29:51 -0400

    fix: Topbar regression; all processes need to know what elements are selected

--- packages/haiku-serialization/src/bll/Row.js
+++ packages/haiku-serialization/src/bll/Row.js
@@ -45,17 +45,23 @@ class Row extends BaseModel {
     return `${this.element.getComponentId()}+${this.host.getComponentId()}-${this.getType()}-${this.getClusterNameString()}-${this.getPropertyNameString()}`
   }
 
-  deselectOthers (metadata) {
+  deselectOthers (metadata, skipSelectElements = false) {
     // Deselect all other rows; currently assume only one row selected at a time
     Row.where({ component: this.component }).forEach((row) => {
       if (row === this) return null
-      row.deselect(metadata)
+      row.deselect(metadata, skipSelectElements)
     })
   }
 
   select (metadata) {
     if (!this._isSelected) {
-      this.deselectOthers(metadata)
+      // The purpose of the `true` argument here tells the instruction to
+      // deselect the other rows, but not deselect all their respective elements;
+      // we need all processes to have a correct record of the actual number of
+      // elements which are explicitly selected on stage, otherwise certain behavior,
+      // such as the topbar controls, will not behave correctly.
+      this.deselectOthers(metadata, true)
+
       this._isSelected = true
       this.emit('update', 'row-selected', metadata)
 
@@ -67,13 +73,13 @@ class Row extends BaseModel {
     return this
   }
 
-  deselect (metadata) {
+  deselect (metadata, skipSelectElements = false) {
     if (this._isSelected) {
       this._isSelected = false
       this.emit('update', 'row-deselected', metadata)
 
       // Roundabout! Note that elements, when unselected, will unselect their corresponding row
-      if (this.isHeading() && this.element && this.element.isSelected()) {
+      if (!skipSelectElements && this.isHeading() && this.element && this.element.isSelected()) {
         this.element.unselect(metadata)
       }
     }
