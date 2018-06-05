5963c57441a2b7867424c8a6b5d96ee2c5393f2a
Sasha Joseph <sasha.joseph@gmail.com>
2018-06-04 17:00:29 -0700

    tweak: restore original logic for alt-selection.

--- packages/haiku-glass/src/react/Glass.js
+++ packages/haiku-glass/src/react/Glass.js
@@ -1539,7 +1539,11 @@ export class Glass extends React.Component {
                   }
                 })
               } else if (!Globals.isControlKeyDown && !Globals.isShiftKeyDown && Globals.isAltKeyDown) { // Alt
-                this.duplicateSelectedElementsThenSelectDuplicates(finish)
+                this.deselectAllOtherElementsIfTargetNotAmongThem(elementTargeted, () => {
+                  this.ensureElementIsSelected(elementTargeted, () => {
+                    this.duplicateSelectedElementsThenSelectDuplicates(finish)
+                  })
+                })
               } else if (!Globals.isControlKeyDown && Globals.isShiftKeyDown && !Globals.isAltKeyDown) { // Shift
                 this.toggleMultiElementSelection(elementTargeted, finish)
               } else if (!Globals.isControlKeyDown && Globals.isShiftKeyDown && Globals.isAltKeyDown) { // Shift+Alt
