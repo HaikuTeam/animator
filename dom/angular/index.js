7978d1a03c9ca2ce813eade64f0c55fc56f11266
Sasha Joseph <sasha.joseph@gmail.com>
2018-06-06 12:29:55 -0700

    fix: ensure Angular instructions actually work.

--- dom/angular/index.js
+++ dom/angular/index.js
@@ -2,5 +2,4 @@
  * Copyright (c) Haiku 2016-2018. All rights reserved.
  */
 
-import HaikuAngularDOMAdapter from '@haiku/core/lib/adapters/angular-dom/HaikuAngularDOMAdapter';
-export default HaikuAngularDOMAdapter;
+module.exports = require('../../lib/adapters/angular-dom/HaikuAngularDOMAdapter').default;
