e79fd5e7243c73bb78949929d8bfb2aa11a20a08
Sasha Joseph <sasha.joseph@gmail.com>
2018-06-08 13:51:12 -0700

    fix: for Bug: Figma Shadows (704591600367406)

---
+++ packages/haiku-serialization/src/svg/plugins/haikuClean.js
@@ -0,0 +1,14 @@
+/**
+ * Custom svgo plugin for cleaning Haiku instantiated components.
+ * @param item
+ * @see {@link https://github.com/svg/svgo/blob/master/docs/how-it-works/en.md}
+ */
+module.exports = {
+  type: 'perItem',
+  fn: (item) => {
+    // Clobber font-family on any/all nodes that try to declare it so users don't get their hopes up.
+    if (item.hasAttr('font-family')) {
+      item.attr('font-family').value = 'Helvetica, Arial, sans-serif'
+    }
+  }
+}
