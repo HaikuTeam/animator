590d37258b17f3838992fdc9d1e6e5eefc71817c
Matthew Trost <mrtrost@gmail.com>
2018-06-26 20:08:58 -0400

    chore: Add demo files

---
+++ packages/@haiku/core/demo/projects/layouts-2/code/child/code.js
@@ -0,0 +1,295 @@
+var Haiku = require("@haiku/core");
+module.exports = {
+  metadata: {
+    folder: "/Users/matthew/Code/HaikuTeam/mono/packages/haiku-plumbing/test/fixtures/projects/blank-project/",
+    uuid: "HAIKU_SHARE_UUID",
+    core: "3.5.0",
+    player: "3.5.0",
+    username: "matthew+2@haiku.ai",
+    organization: "matthew2",
+    project: "UsersMatthewHaikuProjects",
+    branch: "master",
+    version: "0.0.0",
+    title: "Child",
+    type: "haiku",
+    relpath: "code/child/code.js"
+  },
+  options: {},
+  states: {},
+  eventHandlers: {},
+  timelines: {
+    Default: {
+      "haiku:a3d8c7fdf7c8": {
+        "style.WebkitTapHighlightColor": { "0": { value: "rgba(0,0,0,0)" } },
+        "style.transformStyle": { "0": { value: "flat" } },
+        "style.perspective": { "0": { value: "none" } },
+        "style.position": { "0": { value: "relative" } },
+        "style.overflowX": { "0": { value: "visible" } },
+        "style.overflowY": { "0": { value: "visible" } },
+        "sizeAbsolute.x": { "0": { value: 184.861, edited: true } },
+        "sizeAbsolute.y": { "0": { value: 133.967, edited: true } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "sizeMode.z": { "0": { value: 1 } }
+      },
+      "haiku:b46488f4b1ce": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: Haiku.inject(function($container){
+          return $container.size.x
+        },'$container'), edited: true } },
+        "translation.y": { "0": { value: 0, edited: true } },
+        "style.zIndex": { "0": { value: 1 } }
+      },
+      "haiku:2c1baed30226": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:66f20661f228": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#003EDB" } },
+        x: { "0": { value: "0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      },
+      "haiku:788e5e18a149": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: Haiku.inject(function($container){
+          return $container.size.x * 0.25
+        },'$container'), edited: true } },
+        "translation.y": { "0": { value: 0, edited: true } },
+        "style.zIndex": { "0": { value: 2 } }
+      },
+      "haiku:f8f0ffe3816d": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:74a53ca12a68": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#00A94E" } },
+        x: { "0": { value: "0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      },
+      "haiku:fd04f5928e34": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: Haiku.inject(function($container){
+          return $container.size.x * 0.5
+        },'$container'), edited: true } },
+        "translation.y": { "0": { value: 0, edited: true } },
+        "style.zIndex": { "0": { value: 3 } }
+      },
+      "haiku:874e94b00436": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:9ebd7a51a3f6": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#D8D8D8" } },
+        x: { "0": { value: "0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      },
+      "haiku:d160ed46e6e9": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: Haiku.inject(function($container){
+          return $container.size.x * 0.75
+        },'$container'), edited: true } },
+        "translation.y": { "0": { value: 0, edited: true } },
+        "style.zIndex": { "0": { value: 4 } }
+      },
+      "haiku:940aab0549e9": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:61dc890bc77c": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#FF0000" } },
+        x: { "0": { value: "-0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      }
+    }
+  },
+  template: {
+    elementName: "div",
+    attributes: { "haiku-id": "a3d8c7fdf7c8", "haiku-title": "Child" },
+    children: [
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "d160ed46e6e9",
+          "haiku-title": "RectangleR",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleR.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: {
+              "haiku-id": "940aab0549e9",
+              "haiku-title": "Page-1",
+              id: "ee1e3023387a"
+            },
+            children: [
+              {
+                elementName: "rect",
+                attributes: {
+                  "haiku-id": "61dc890bc77c",
+                  "haiku-title": "Rectangle",
+                  id: "a6ca08775d84"
+                },
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "fd04f5928e34",
+          "haiku-title": "RectangleO",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleO.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: {
+              "haiku-id": "874e94b00436",
+              "haiku-title": "Page-1",
+              id: "912916e8902c"
+            },
+            children: [
+              {
+                elementName: "rect",
+                attributes: {
+                  "haiku-id": "9ebd7a51a3f6",
+                  "haiku-title": "Rectangle",
+                  id: "61189b78d926"
+                },
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "788e5e18a149",
+          "haiku-title": "RectangleG",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleG.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: {
+              "haiku-id": "f8f0ffe3816d",
+              "haiku-title": "Page-1",
+              id: "d30964ff0beb"
+            },
+            children: [
+              {
+                elementName: "rect",
+                attributes: {
+                  "haiku-id": "74a53ca12a68",
+                  "haiku-title": "Rectangle",
+                  id: "2b9f916707eb"
+                },
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "b46488f4b1ce",
+          "haiku-title": "RectangleB",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleB.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: {
+              "haiku-id": "2c1baed30226",
+              "haiku-title": "Page-1",
+              id: "1acd2598fd2c"
+            },
+            children: [
+              {
+                elementName: "rect",
+                attributes: {
+                  "haiku-id": "66f20661f228",
+                  "haiku-title": "Rectangle",
+                  id: "f0d53a70bc8d"
+                },
+                children: []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+};
---
+++ packages/@haiku/core/demo/projects/layouts-2/code/main/code.js
@@ -0,0 +1,283 @@
+var Haiku = require("@haiku/core");
+var _code_child_code = require("./../child/code.js");
+module.exports = {
+  metadata: {
+    folder: "/Users/matthew/Code/HaikuTeam/mono/packages/haiku-plumbing/test/fixtures/projects/blank-project/",
+    uuid: "HAIKU_SHARE_UUID",
+    core: "3.5.0",
+    player: "3.5.0",
+    username: "matthew+2@haiku.ai",
+    organization: "matthew2",
+    project: "UsersMatthewHaikuProjects",
+    branch: "master",
+    version: "0.0.0",
+    title: "Main",
+    type: "haiku",
+    relpath: "code/main/code.js"
+  },
+  options: {},
+  states: {},
+  eventHandlers: {},
+  timelines: {
+    Default: {
+      "haiku:03757d2ca102": {
+        "style.WebkitTapHighlightColor": { "0": { value: "rgba(0,0,0,0)" } },
+        "style.transformStyle": { "0": { value: "flat" } },
+        "style.perspective": { "0": { value: "none" } },
+        "style.position": { "0": { value: "relative" } },
+        "style.overflowX": { "0": { value: "hidden" } },
+        "style.overflowY": { "0": { value: "hidden" } },
+        "sizeAbsolute.x": { "0": { value: 550 } },
+        "sizeAbsolute.y": { "0": { value: 400 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "sizeMode.z": { "0": { value: 1 } }
+      },
+      "haiku:476788f335c3": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "sizeAbsolute.x": { "0": { value: "auto" } },
+        "sizeAbsolute.y": { "0": { value: "auto" } },
+        "translation.x": { "0": { value: 299 } },
+        "translation.y": { "0": { value: 199 } },
+        "sizeMode.z": { "0": { value: 1 } },
+        "origin.x": { "0": { value: 0.5 } },
+        "origin.y": { "0": { value: 0.5 } },
+        "style.zIndex": { "0": { value: 1 } },
+        playback: { "0": { value: "loop" } }
+      },
+      "haiku:e1898f413657": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: 71.5 } },
+        "translation.y": { "0": { value: 154 } },
+        "style.zIndex": { "0": { value: 2 } }
+      },
+      "haiku:b86b2d8c20dd": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:4de8aa136233": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#003EDB" } },
+        x: { "0": { value: "0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      },
+      "haiku:2142043393ac": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: 91.5 } },
+        "translation.y": { "0": { value: 197 } },
+        "style.zIndex": { "0": { value: 3 } }
+      },
+      "haiku:0e6f6e1ba7d4": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:f261bcf58bd5": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#00A94E" } },
+        x: { "0": { value: "0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      },
+      "haiku:c6128ed81561": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: 108.5, edited: true } },
+        "translation.y": { "0": { value: 240, edited: true } },
+        "style.zIndex": { "0": { value: 4 } }
+      },
+      "haiku:5e3327f6ad03": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:01e3977aa273": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#D8D8D8" } },
+        x: { "0": { value: "0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      },
+      "haiku:73e46f4b2971": {
+        "style.position": { "0": { value: "absolute" } },
+        "style.margin": { "0": { value: "0" } },
+        "style.padding": { "0": { value: "0" } },
+        "style.border": { "0": { value: "0" } },
+        "sizeAbsolute.x": { "0": { value: 52 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 36 } },
+        "sizeMode.y": { "0": { value: 1 } },
+        "translation.x": { "0": { value: 127.5, edited: true } },
+        "translation.y": { "0": { value: 285, edited: true } },
+        "style.zIndex": { "0": { value: 5 } }
+      },
+      "haiku:d9b9a4ccc892": {
+        stroke: { "0": { value: "none" } },
+        strokeWidth: { "0": { value: "1" } },
+        fill: { "0": { value: "none" } },
+        fillRule: { "0": { value: "evenodd" } }
+      },
+      "haiku:3b3c21658971": {
+        stroke: { "0": { value: "#979797" } },
+        fill: { "0": { value: "#FF0000" } },
+        x: { "0": { value: "-0.5" } },
+        y: { "0": { value: "0.5" } },
+        "sizeAbsolute.x": { "0": { value: 51 } },
+        "sizeMode.x": { "0": { value: 1 } },
+        "sizeAbsolute.y": { "0": { value: 35 } },
+        "sizeMode.y": { "0": { value: 1 } }
+      }
+    }
+  },
+  template: {
+    elementName: "div",
+    attributes: { "haiku-id": "03757d2ca102", "haiku-title": "Main" },
+    children: [
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "73e46f4b2971",
+          "haiku-title": "RectangleR",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleR.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: { "haiku-id": "d9b9a4ccc892", id: "Page-1" },
+            children: [
+              {
+                elementName: "rect",
+                attributes: { "haiku-id": "3b3c21658971", id: "Rectangle" },
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "c6128ed81561",
+          "haiku-title": "RectangleO",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleO.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: { "haiku-id": "5e3327f6ad03", id: "Page-1" },
+            children: [
+              {
+                elementName: "rect",
+                attributes: { "haiku-id": "01e3977aa273", id: "Rectangle" },
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "2142043393ac",
+          "haiku-title": "RectangleG",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleG.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: { "haiku-id": "0e6f6e1ba7d4", id: "Page-1" },
+            children: [
+              {
+                elementName: "rect",
+                attributes: { "haiku-id": "f261bcf58bd5", id: "Rectangle" },
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        elementName: "svg",
+        attributes: {
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink",
+          "haiku-id": "e1898f413657",
+          "haiku-title": "RectangleB",
+          "haiku-source": "designs/UsersMatthewHaikuPro.sketch.contents/slices/RectangleB.svg"
+        },
+        children: [
+          {
+            elementName: "g",
+            attributes: { "haiku-id": "b86b2d8c20dd", id: "Page-1" },
+            children: [
+              {
+                elementName: "rect",
+                attributes: { "haiku-id": "4de8aa136233", id: "Rectangle" },
+                children: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        elementName: _code_child_code,
+        attributes: {
+          "haiku-id": "476788f335c3",
+          "haiku-var": "_code_child_code",
+          "haiku-title": "Child",
+          "haiku-source": "./code/child/code.js"
+        },
+        children: []
+      }
+    ]
+  }
+};
