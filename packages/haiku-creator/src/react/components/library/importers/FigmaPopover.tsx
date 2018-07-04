ec2875bed6a74890476ff23cd6d44bfad6cad35c
Roberto Dip <dip.jesusr@gmail.com>
2018-07-04 15:49:54 -0300

    wip: move importers/FigmaPopover to TS

--- packages/haiku-creator/src/react/components/library/importers/FigmaPopover.tsx
+++ packages/haiku-creator/src/react/components/library/importers/FigmaPopover.tsx
@@ -1,30 +1,38 @@
+import {Experiment, experimentIsEnabled} from 'haiku-common/lib/experiments';
 import * as React from 'react';
+// @ts-ignore
 import * as Popover from 'react-popover';
 import FigmaForm from './FigmaForm';
 
-class FigmaPopover extends React.PureComponent {
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      isPopoverOpen: false,
-    };
-  }
+class FigmaPopover extends React.PureComponent<any, any> {
+  state = {
+    isPopoverOpen: false,
+  };
 
   showPopover () {
     this.setState({isPopoverOpen: true});
   }
 
-  hidePopover () {
+  hidePopover = () => {
     this.setState({isPopoverOpen: false});
-  }
+  };
+
+  doubleClickToOpen = () => {
+    if (!experimentIsEnabled(Experiment.CleanInitialLibraryState)) {
+      this.showPopover();
+    }
+  };
+
+  clickToOpen = () => {
+    if (experimentIsEnabled(Experiment.CleanInitialLibraryState)) {
+      this.showPopover();
+    }
+  };
 
   render () {
     return (
       <Popover
-        onOuterAction={() => {
-          this.hidePopover();
-        }}
+        onOuterAction={this.hidePopover}
         isOpen={this.state.isPopoverOpen}
         place="below"
         tipSize={0.01}
@@ -33,15 +41,14 @@ class FigmaPopover extends React.PureComponent {
             figma={this.props.figma}
             onAskForFigmaAuth={this.props.onAskForFigmaAuth}
             onImportFigmaAsset={this.props.onImportFigmaAsset}
-            onPopoverHide={() => {
-              this.hidePopover();
-            }}
+            onPopoverHide={this.hidePopover}
           />
         }
       >
-        <span onDoubleClick={() => {
-          this.showPopover();
-        }}>
+        <span
+          onDoubleClick={this.doubleClickToOpen}
+          onClick={this.clickToOpen}
+        >
           {this.props.children}
         </span>
       </Popover>
