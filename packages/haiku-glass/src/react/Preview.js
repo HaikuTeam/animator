856bbfad5be57289ac33132aa0cb17cbaf80872b
Matthew Trost <mrtrost@gmail.com>
2018-07-25 19:11:48 -0400

    fix: Only emit/log state:change if state changed (757025233282106)

--- packages/haiku-glass/src/react/Preview.js
+++ packages/haiku-glass/src/react/Preview.js
@@ -27,6 +27,11 @@ export default class Preview extends React.Component {
     );
 
     this.component.on('state:change', (attachedObject) => {
+      // Don't log if the values are already equivalent (unchanged)
+      if (attachedObject.to === attachedObject.from) {
+        return;
+      }
+
       let message = '';
       if (attachedObject.queued) {
         message = `State transition ${attachedObject.state} to target ${attachedObject.to} with duration ${attachedObject.duration} queued`;
