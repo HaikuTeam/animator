3785e498ac59e2b434db8c19a1558b425af21272
jonaias <jonas.jonaias@gmail.com>
2018-08-03 16:38:25 -0300

    Cleanup before fixing code editor popups

--- src/helpers/interactionModes.ts
+++ src/helpers/interactionModes.ts
@@ -8,8 +8,9 @@
  */
 // tslint:disable-next-line:no-namespace
 export namespace InteractionMode {
-  export const EDIT = 0; // Default mode, all is editable
-  export const LIVE = 1; // Preview mode
+  export const GLASS_EDIT = 0; // Default mode, all is editable
+  export const GLASS_LIVE = 1; // Preview mode
+  export const CODE_EDITOR = 2; // Code editor mode
 }
 
 /**
@@ -20,7 +21,19 @@ export namespace InteractionMode {
  * @returns boolean
  */
 export function isPreviewMode (mode): boolean {
-  return mode === InteractionMode.LIVE;
+  return mode === InteractionMode.GLASS_LIVE;
+}
+
+export function isEditMode (mode): boolean {
+  return mode === InteractionMode.GLASS_EDIT;
+}
+
+export function isCodeEditorMode (mode): boolean {
+  return mode === InteractionMode.CODE_EDITOR;
+}
+
+export function showGlassOnStage (mode): boolean {
+  return isPreviewMode(mode) || isEditMode(mode);
 }
 
 /**
