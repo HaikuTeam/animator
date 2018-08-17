270a9a2480afdd2fcb3b1c4ac14a2170aa8ed5b5
Roberto Dip <rroperzh@gmail.com>
2018-08-17 16:16:48 -0300

    fix: update the max keyframe area when a new keyframe is added

--- packages/haiku-timeline/src/components/PostMaxKeyframeArea.jsx
+++ packages/haiku-timeline/src/components/PostMaxKeyframeArea.jsx
@@ -4,14 +4,29 @@ import zIndex from './styles/zIndex';
 import Palette from 'haiku-ui-common/lib/Palette';
 
 export default class PostMaxKeyframeArea extends React.Component {
-  render () {
-    const frameInfo = this.props.timeline.getFrameInfo();
+  constructor (props) {
+    super();
 
+    this.frameInfo = props.timeline.getFrameInfo();
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // FIXME: when a keyframe is added, it takes some time for the Timeline
+    // model to clear the cached value of frameInfo, this hack re-renders
+    // the component after 100ms after new props are arrived to compensate that
+    // delay...
+    setTimeout(() => {
+      this.frameInfo = nextProps.timeline.getFrameInfo();
+      this.forceUpdate();
+    }, 100);
+  }
+
+  render () {
     return (
       <div style={{
         background: Color(Palette.COAL).fade(0.8),
         zIndex: zIndex.areaPostMaxKeyframe.base,
-        marginLeft: frameInfo.maxf * frameInfo.pxpf + this.props.propertiesPixelWidth + 14,
+        marginLeft: this.frameInfo.maxf * this.frameInfo.pxpf + this.props.propertiesPixelWidth + 14,
         position: 'sticky',
         pointerEvents: 'none',
         height: 'calc(100% - 35px)',
