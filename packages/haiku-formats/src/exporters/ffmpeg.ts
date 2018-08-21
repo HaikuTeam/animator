28c1394413c4dbd9a9abab633a19a6290695ac3f
Sasha Joseph <sasha.joseph@gmail.com>
2018-08-20 17:38:24 -0700

    chore: add comment explaining our ffmpeg hack.

--- packages/haiku-formats/src/exporters/ffmpeg.ts
+++ packages/haiku-formats/src/exporters/ffmpeg.ts
@@ -1,6 +1,17 @@
 import fluentFfmpeg = require('fluent-ffmpeg');
 import {join} from 'path';
 
+/**
+ * Note to future explorers:
+ *
+ * We have to do some acrobatics here to resolve the linked ffmpeg executable from ffmpeg-static.
+ *
+ * In dev mode, we can rely on the the ffmpeg-static package to produce its own path reliably.
+ *
+ * In prod, we don't even bother package ffmpeg-static, and instead hoist its binary to sit parallel to the app.asar
+ * directory where our actual code lives. For numerous reasons, electron-builder's asarUnpack option turned out not
+ * to be suitable for this purpose.
+ */
 try {
   // @ts-ignore
   // tslint:disable-next-line:no-var-requires
