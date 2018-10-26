2bd30b966def5986e6af6aadafcf8fe09a0f3325
Sasha Joseph <sasha.joseph@gmail.com>
2018-10-26 14:07:08 -0700

    fix: force groups in Lottie to have integer dimensions.

--- packages/haiku-formats/src/exporters/bodymovin/bodymovinExporter.ts
+++ packages/haiku-formats/src/exporters/bodymovin/bodymovinExporter.ts
@@ -525,8 +525,8 @@ export class BodymovinExporter extends BaseExporter implements ExporterInterface
         ...this.standardTransformsForTimeline(timeline),
         ...this.transformsForLayerTimeline(timeline),
       },
-      [LayerKey.Width]: initialValueOr(timeline, 'sizeAbsolute.x', 0),
-      [LayerKey.Height]: initialValueOr(timeline, 'sizeAbsolute.y', 0),
+      [LayerKey.Width]: Math.round(initialValueOr(timeline, 'sizeAbsolute.x', 0)),
+      [LayerKey.Height]: Math.round(initialValueOr(timeline, 'sizeAbsolute.y', 0)),
     });
 
     this.layerStack.set(node, layers);
