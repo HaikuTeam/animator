857bf3daf079448a9b9b71c5e2dcfad19416a454
Sasha Joseph <sasha.joseph@gmail.com>
2018-10-26 14:20:18 -0700

    fix: ensure fill is a string before trying to match it.

--- packages/haiku-formats/src/exporters/bodymovin/bodymovinExporter.ts
+++ packages/haiku-formats/src/exporters/bodymovin/bodymovinExporter.ts
@@ -927,7 +927,7 @@ export class BodymovinExporter extends BaseExporter implements ExporterInterface
       toJSON: lottieAndroidStreamSafeToJson,
     };
 
-    const matches = getValueReferenceMatchArray(initialValue(timeline, 'fill'));
+    const matches = getValueReferenceMatchArray(initialValue(timeline, 'fill') + '');
 
     if (matches !== null) {
       // We matched a value reference, e.g. something like `fill ='url(#foobar)'`. This means we are dealing with a
