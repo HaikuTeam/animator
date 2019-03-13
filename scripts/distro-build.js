0fa1fe0de9404ffa68a78563b8c20912f4528dbc
Sasha Joseph <sasha.joseph@gmail.com>
2019-03-12 19:57:24 -0700

    fix: work around corrupted zip archive from latest build stack by zipping manually on mac

--- scripts/distro-build.js
+++ scripts/distro-build.js
@@ -1,6 +1,7 @@
 const fse = require('fs-extra');
 const cp = require('child_process');
 const path = require('path');
+const nowVersion = require('./helpers/nowVersion');
 // This line should be commented to build locally on windows
 const deploy = require('./deploy');
 const forceNodeEnvProduction = require('./helpers/forceNodeEnvProduction');
@@ -10,13 +11,18 @@ forceNodeEnvProduction();
 
 const ROOT = global.process.cwd();
 
-let platform = process.env.HAIKU_RELEASE_PLATFORM;
+const platform = process.env.HAIKU_RELEASE_PLATFORM;
 switch (platform) {
   case 'mac':
     process.env.CSC_LINK = `file://${deploy.vault}/${deploy.certificate}`;
     process.env.CSC_KEY_PASSWORD = fse.readFileSync(path.join(deploy.vault, `${deploy.certificate}.password`)).toString().trim();
 
     cp.execSync(`./node_modules/.bin/build --mac --publish=never`, {cwd: ROOT, stdio: 'inherit'});
+    // The latest build chain breaks our zip archive, so we need to manually zip it.
+    const distRoot = path.resolve(ROOT, 'dist');
+    const {productName} = require(path.join(ROOT, 'package.json')).build;
+    const zipTarget = path.join(distRoot, `${productName}-${nowVersion()}-mac.zip`);
+    cp.execSync(`/usr/bin/zip -r -7 '${zipTarget}' Haiku.app`, {cwd: path.join(distRoot, 'mac'), stdio: 'inherit'});
     break;
   case 'windows':
     // process.env.WIN_CSC_LINK = `file://${deploy.vault}/${deploy.certificate}`
