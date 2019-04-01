ec42b7b10f647de3c7b95bcb29825df16504b14a
Roberto Dip <dip.jesusr@gmail.com>
2019-04-01 22:54:07 +0200

    chore: use `productName` var on distro scripts

--- scripts/distro-open.js
+++
@@ -1,4 +0,0 @@
-const cp = require('child_process');
-const path = require('path');
-let ROOT = path.join(__dirname, '..');
-cp.execSync('open ./dist/mac/Haiku.app/Contents/MacOS/Haiku', {cwd: ROOT, stdio: 'inherit'});
