0ff9f21cc49f040056c59bf2e3594e162e87d43f
Gabriel Obaldía <gobaldia@gmail.com>
2020-04-08 14:58:21 -0700

    Hid sketch references for Windows (#1025)
    * Refactor
    
    * Hid sketch references for Windows

--- packages/haiku-creator/src/react/components/library/AssetItem.js
+++ packages/haiku-creator/src/react/components/library/AssetItem.js
@@ -181,6 +181,10 @@ class AssetItem extends React.Component {
     this.endDragInCaseItWasStartedInadvertently();
   }
 
+  considerSketch() {
+    return isMac() && this.props.asset.isSketchFile();
+  }
+
   handleShowAsset () {
     shell.showItemInFolder(this.props.asset.getAbspath());
     this.endDragInCaseItWasStartedInadvertently();
@@ -242,7 +246,7 @@ class AssetItem extends React.Component {
     }
 
     // Only display Open In Sketch on mac
-    if (isMac() && this.props.asset.isSketchFile()) {
+    if (this.considerSketch()) {
       items.push({
         label: 'Open In Sketch',
         icon: SketchIconSVG,
@@ -271,7 +275,7 @@ class AssetItem extends React.Component {
     // Things like built-in components can't be deleted or shown in finder
     if (!this.props.asset.isRemoteAsset() && !this.props.asset.isFigmaFile()) {
       items.push({
-        label: 'Show In Finder',
+        label: isWindows() ? 'Show in File Explorer' : 'Show In Finder',
         icon: FolderIconSVG,
         onClick: this.handleShowAsset.bind(this),
       });
@@ -329,7 +333,7 @@ class AssetItem extends React.Component {
     }
 
     if (
-      this.props.asset.isSketchFile() ||
+      this.considerSketch() ||
       this.isFigmaAndCanBeOpened() ||
       this.props.asset.isIllustratorFile() ||
       this.props.asset.isOrphanSvg() ||
@@ -386,7 +390,7 @@ class AssetItem extends React.Component {
       );
     }
 
-    if (this.props.asset.isSketchFile()) {
+    if (this.considerSketch()) {
       return (
         <span
           className="sketch-icon-container"
@@ -493,7 +497,7 @@ class AssetItem extends React.Component {
       return 'Double click to open in Illustrator';
     }
 
-    if (this.props.asset.isSketchFile()) {
+    if (this.considerSketch()) {
       return 'Double click to open in Sketch';
     }
 
@@ -502,7 +506,13 @@ class AssetItem extends React.Component {
     }
 
     if (this.props.asset.isDesignsHostFolder()) {
-      return 'Your design assets — import from Sketch, Figma, or Illustrator';
+      if (isMac()) {
+        return 'Your design assets — import from Sketch, Figma, or Illustrator';
+      }
+      else {
+        return 'Your design assets — import from Figma or Illustrator';
+      }
+      
     }
 
     if (this.props.asset.isDraggable()) {
@@ -548,7 +558,7 @@ class AssetItem extends React.Component {
       `;
     }
 
-    if (this.props.asset.isSketchFile()) {
+    if (this.considerSketch()) {
       return `
         ⇧ Double click to open this file in Sketch.
         Slices and artboards will sync when you save.
