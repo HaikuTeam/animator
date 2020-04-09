da2ea9f0ba4df781a4b52455438ea51cd57c2b0a
Roberto Dip <rob@haiku.ai>
2020-04-09 19:55:21 -0300

    ops: notarize the macOS app before bundling

---
+++ scripts/notarize.js
@@ -0,0 +1,25 @@
+const {notarize} = require('electron-notarize');
+
+if (!process.env.APPLE_NOTARIZATION_API_KEY) {
+  throw new Error('env var missing');
+}
+if (!process.env.APPLE_NOTARIZATION_API_ISSUER) {
+  throw new Error('env var missing');
+}
+
+exports.default = function notarizing(context) {
+  const {electronPlatformName, appOutDir} = context;
+
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'com.Haiku.HaikuForDesignersAndEngineers',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleApiKey: process.env.APPLE_NOTARIZATION_API_KEY,
+    appleApiIssuer: process.env.APPLE_NOTARIZATION_API_ISSUER,
+  });
+};
