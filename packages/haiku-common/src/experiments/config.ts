610e15a3c60f40c24ded8d0702a638cec1590c5b
Roberto Dip <rob@haiku.ai>
2020-04-13 13:55:40 -0700

    fix(windows): safely set path to experiments to avoid build crashes

--- packages/haiku-common/src/experiments/config.ts
+++ packages/haiku-common/src/experiments/config.ts
@@ -1,4 +1,6 @@
+import {join} from 'path';
+
 /**
  * Retrieve the experiment config from disk.
  */
-export const getExperimentConfig = () => require('../../config/experiments.json');
+export const getExperimentConfig = () => require(join('..', '..', 'config', 'experiments.json'));
