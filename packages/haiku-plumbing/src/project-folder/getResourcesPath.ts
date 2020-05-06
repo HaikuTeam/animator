2e2e052f99156f70151609362eb03f1e2f7a6a31
Roberto Dip <rob@haiku.ai>
2020-05-06 20:57:23 -0300

    wip: remove completed TODO

--- packages/haiku-plumbing/src/project-folder/getResourcesPath.ts
+++ packages/haiku-plumbing/src/project-folder/getResourcesPath.ts
@@ -1,11 +1,6 @@
 import {isMac} from 'haiku-common/lib/environments/os';
 import {join} from 'path';
 
-// TODO:
-// - Ensure we are using the correct path in Windows.
-// - If clean up if the route to the resources path is the same in both platforms.
-// - Add a note stating why we have to do this manually
-
 /**
  * Returns the path to the resources directory. You can define resources in the
  * root `package.json` file under `extraResources`
